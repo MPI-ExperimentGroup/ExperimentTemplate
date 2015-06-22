1bdac69c5f2b507a7c825887937348cef16fb9fa
Peter Withers <peter.withers@mpi.nl>
2015-06-22 13:32:37 +0200

    Refactored the package structure gradually so that Git has more of a chance to track file name changes.

--- gwt-cordova/src/main/java/nl/mpi/tg/eg/experiment/client/model/RoundSample.java
+++ gwt-cordova/src/main/java/nl/mpi/tg/eg/experiment/client/model/RoundSample.java
@@ -15,9 +15,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-package nl.ru.languageininteraction.language.client.model;
+package nl.mpi.tg.eg.experiment.client.model;
 
-import nl.ru.languageininteraction.language.client.LanguageDataProvider.LanguageSample;
+import nl.mpi.tg.eg.experiment.client.LanguageDataProvider.LanguageSample;
 
 /**
  * @since Jan 29, 2015 12:59:31 PM (creation date)
