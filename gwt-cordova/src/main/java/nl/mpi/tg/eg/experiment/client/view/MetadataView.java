bad2c61fb7391102d01390c7cc89290e231a43f6
Peter Withers <peter.withers@mpi.nl>
2015-06-26 17:13:22 +0200

    Refactored the package structure gradually so that Git has more of a chance to track file name changes.

--- gwt-cordova/src/main/java/nl/mpi/tg/eg/experiment/client/view/MetadataView.java
+++ gwt-cordova/src/main/java/nl/mpi/tg/eg/experiment/client/view/MetadataView.java
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-package nl.ru.languageininteraction.language.client.view;
+package nl.mpi.tg.eg.experiment.client.view;
 
 import nl.mpi.tg.eg.experiment.client.view.ComplexView;
 import com.google.gwt.event.dom.client.BlurEvent;
