4170bc111be92e4dd4db10258a6a556fd9b06d0e
Peter Withers <peter.withers@mpi.nl>
2015-07-03 13:59:14 +0200

    Refactored the package structure gradually so that Git has more of a chance to track file name changes.

--- gwt-cordova/src/main/java/nl/mpi/tg/eg/experiment/client/service/HighScoreService.java
+++ gwt-cordova/src/main/java/nl/mpi/tg/eg/experiment/client/service/HighScoreService.java
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-package nl.ru.languageininteraction.language.client.registration;
+package nl.mpi.tg.eg.experiment.client.service;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.JsArray;
