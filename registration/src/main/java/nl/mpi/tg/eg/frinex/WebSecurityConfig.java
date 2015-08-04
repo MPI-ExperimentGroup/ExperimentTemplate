6e7dec0d4989993a91b8ed172d0fba2ddee55008
Peter Withers <peter.withers@mpi.nl>
2015-08-04 17:35:13 +0200

    Added clickable table rows with CSS hover. Added CSS styling to the login form.

--- registration/src/main/java/nl/mpi/tg/eg/frinex/WebSecurityConfig.java
+++ registration/src/main/java/nl/mpi/tg/eg/frinex/WebSecurityConfig.java
@@ -45,7 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/screenChange", "/timeStamp", "/metadata", "/tagEvent", "/tagPairEvent", "/index.html").permitAll()
+                .antMatchers("/screenChange", "/timeStamp", "/metadata", "/tagEvent", "/tagPairEvent", "/adminpages.css").permitAll()
                 .anyRequest().authenticated()
                 .and().formLogin().loginPage("/login").permitAll()
                 .and().logout().permitAll();
