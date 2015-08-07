951c9adc2ef76d5a40bc1a1fb389cfa52d42864f
Peter Withers <peter.withers@mpi.nl>
2015-08-07 18:36:31 +0200

    Updated the configuration file so that a simple demo application can be produced. Updated the cordova configuration so a demo mobile application can be built.

--- gwt-cordova/setup-cordova.sh
+++ gwt-cordova/setup-cordova.sh
@@ -1,6 +1,6 @@
 #mvn install
 cd target
-appname=lingquest-0.1.353-stable
+appname=frinexweb-0.1.457-testing
 rm -rf $appname-cordova
 unzip $appname-cordova.zip -d $appname-cordova
 cd $appname-cordova
