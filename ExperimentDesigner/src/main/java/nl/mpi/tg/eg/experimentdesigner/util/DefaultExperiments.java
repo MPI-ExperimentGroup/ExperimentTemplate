1ae8e923e38f76d6cf83447fcc42ca40fb7b60e1
Peter Withers <peter.withers@mpi.nl>
2015-09-15 15:20:08 +0200

    Added more content to the default experiment data.

--- ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/util/DefaultExperiments.java
+++ ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/util/DefaultExperiments.java
@@ -22,7 +22,12 @@ import nl.mpi.tg.eg.experimentdesigner.dao.ExperimentRepository;
 import nl.mpi.tg.eg.experimentdesigner.dao.MetadataRepository;
 import nl.mpi.tg.eg.experimentdesigner.dao.PresenterFeatureRepository;
 import nl.mpi.tg.eg.experimentdesigner.dao.PresenterScreenRepository;
+import nl.mpi.tg.eg.experimentdesigner.model.FeatureAttribute;
+import nl.mpi.tg.eg.experimentdesigner.model.FeatureType;
 import nl.mpi.tg.eg.experimentdesigner.model.Metadata;
+import nl.mpi.tg.eg.experimentdesigner.model.PresenterFeature;
+import nl.mpi.tg.eg.experimentdesigner.model.PresenterScreen;
+import nl.mpi.tg.eg.experimentdesigner.model.PresenterType;
 
 /**
  * @since Sep 8, 2015 3:19:56 PM (creation date)
@@ -41,12 +46,34 @@ public class DefaultExperiments {
         experiment.setDataSubmitUrl("http://ems12.mpi.nl/dobes-frinex-admin-0.1.38-testing/");
         experiment.setStaticFilesUrl("static/");
 
-        final Metadata metadata = new Metadata("postName", "registrationField", "controlledRegex", "controlledMessage", true, "duplicatesControlledMessage");
-        final Metadata metadata1 = new Metadata("postName1", "registrationField1", "controlledRegex1", "controlledMessage1", true, "duplicatesControlledMessage1");
-        metadataRepository.save(metadata);
-        metadataRepository.save(metadata1);
+        final Metadata metadata = new Metadata("workerId", "Reporter name *", ".'{'3,'}'", "Please enter at least three letters.", true, "This test can only be done once per worker.");
+        final Metadata metadata1 = new Metadata("errordevice", "Device model", ".'{'2,'}'", "Please enter the device model", false, null);
+        final Metadata metadata2 = new Metadata("errordescription", "Please describe the error", ".'{'2,'}'", "Please enter a short description of the issue", false, null);
         experiment.getMetadata().add(metadata);
         experiment.getMetadata().add(metadata1);
+        experiment.getMetadata().add(metadata2);
+        metadataRepository.save(experiment.getMetadata());
+        experiment.getPresenterScreen().add(addVideosMenu(presenterFeatureRepository));
+        presenterScreenRepository.save(experiment.getPresenterScreen());
         experimentRepository.save(experiment);
+    }
+
+    private PresenterScreen addVideosMenu(PresenterFeatureRepository presenterFeatureRepository) {
+        final PresenterScreen presenterScreen = new PresenterScreen("Video List", "Videos", "AutoMenu", "VideosPage", null, PresenterType.text);
+        presenterScreen.getPresenterFeatureList().add(new PresenterFeature(FeatureType.htmlText, "This is a simple video codec testing application."));
+        presenterScreen.getPresenterFeatureList().add(new PresenterFeature(FeatureType.padding, null));
+        final PresenterFeature presenterFeature = new PresenterFeature(FeatureType.AudioRecorderPanel, null);
+        presenterFeature.addFeatureAttributes(FeatureType.AudioRecorderPanel.getFeatureAttributes()[0], "some path");
+        presenterScreen.getPresenterFeatureList().add(presenterFeature);
+
+        final PresenterFeature optionButton1 = new PresenterFeature(FeatureType.optionButton, "Video Test Page (aspen)");
+        optionButton1.addFeatureAttributes(FeatureAttribute.target, "VideoTestPageAspen");
+        presenterScreen.getPresenterFeatureList().add(optionButton1);
+
+        presenterScreen.getPresenterFeatureList().add(new PresenterFeature(FeatureType.padding, null));
+        presenterScreen.getPresenterFeatureList().add(new PresenterFeature(FeatureType.versionData, null));
+
+        presenterFeatureRepository.save(presenterScreen.getPresenterFeatureList());
+        return presenterScreen;
     }
 }
