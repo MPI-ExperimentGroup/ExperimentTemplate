911cf7cb4a7e4d21d265568eb84b8e5bdc6677e8
Peter Withers <peter.withers@mpi.nl>
2015-10-06 15:25:28 +0200

    Added a stimulus grid to the video timeline annotator panel.

--- gwt-cordova/src/main/java/nl/mpi/tg/eg/experiment/client/view/AnnotationTimelinePanel.java
+++ gwt-cordova/src/main/java/nl/mpi/tg/eg/experiment/client/view/AnnotationTimelinePanel.java
@@ -17,11 +17,18 @@
  */
 package nl.mpi.tg.eg.experiment.client.view;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.safehtml.shared.UriUtils;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.google.gwt.user.client.ui.ButtonBase;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import nl.mpi.tg.eg.experiment.client.listener.PresenterEventListner;
+import nl.mpi.tg.eg.experiment.client.model.Stimulus;
+import nl.mpi.tg.eg.experiment.client.service.ServiceLocations;
+import nl.mpi.tg.eg.experiment.client.service.StimulusProvider;
 
 /**
  * @since Sep 21, 2015 11:56:46 AM (creation date)
@@ -29,26 +36,36 @@ import com.google.gwt.user.client.ui.VerticalPanel;
  */
 public class AnnotationTimelinePanel extends VerticalPanel {
 
-    public AnnotationTimelinePanel(String width, String poster, String mp4, String ogg, String webm) {
+    private final StimulusProvider stimulusProvider = new StimulusProvider();
+    protected final ServiceLocations serviceLocations = GWT.create(ServiceLocations.class);
+
+    public AnnotationTimelinePanel(String width, String poster, String mp4, String ogg, String webm, Stimulus.Tag tag, int maxStimuli, int columnCount, String imageWidth) {
         final HorizontalPanel horizontalPanel = new HorizontalPanel();
         this.setStylePrimaryName("annotationTimelinePanel");
         final VideoPanel videoPanel = new VideoPanel(width, poster, mp4, ogg, webm);
         horizontalPanel.add(videoPanel);
         final VerticalPanel verticalPanel = new VerticalPanel();
-//        verticalPanel.add(new Button("one", new ClickHandler() {
-//
-//            @Override
-//            public void onClick(ClickEvent event) {
-//                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//            }
-//        }));
-//        verticalPanel.add(new Button("two", new ClickHandler() {
-//
-//            @Override
-//            public void onClick(ClickEvent event) {
-//                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//            }
-//        }));
+        final StimulusGrid stimulusGrid = new StimulusGrid();
+        stimulusProvider.getSubset(tag, maxStimuli, "");
+        int stimulusCounter = 0;
+        while (stimulusProvider.hasNextStimulus()) {
+            stimulusProvider.getNextStimulus();
+            Stimulus stimulus = stimulusProvider.getCurrentStimulus();
+            stimulusGrid.addImageItem(new PresenterEventListner() {
+
+                @Override
+                public String getLabel() {
+                    return null;
+//                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                }
+
+                @Override
+                public void eventFired(ButtonBase button) {
+//                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                }
+            }, UriUtils.fromString(serviceLocations.staticFilesUrl() + stimulus.getImage()), stimulusCounter / columnCount, 1 + stimulusCounter++ % columnCount, imageWidth);
+        }
+        horizontalPanel.add(stimulusGrid);
         horizontalPanel.add(verticalPanel);
         this.add(horizontalPanel);
         final Label timelineCursor = new Label();
