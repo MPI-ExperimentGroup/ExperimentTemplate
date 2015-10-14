413c23f9b6a21f63aa47393a25036a18a74b868d
Peter Withers <peter.withers@mpi.nl>
2015-10-14 16:11:00 +0200

    In the video timeline annotation panel, added highlighting of each stimulus button when the video play position intersects an annotation for that stimulus. Also added annotation lengths on the timeline to match the in and out points for each annotation.

--- gwt-cordova/src/main/java/nl/mpi/tg/eg/experiment/client/model/AnnotationData.java
+++ gwt-cordova/src/main/java/nl/mpi/tg/eg/experiment/client/model/AnnotationData.java
@@ -1,7 +1,19 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2015 Max Planck Institute for Psycholinguistics, Nijmegen
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 package nl.mpi.tg.eg.experiment.client.model;
 
@@ -14,14 +26,16 @@ public class AnnotationData {
     private double inTime = 0;
     private double outTime = 0;
     private String annotationHtml = "";
+    private Stimulus stimulus;
 
     public AnnotationData() {
     }
 
-    public AnnotationData(double inTime, double outTime, String annotationHtml) {
+    public AnnotationData(double inTime, double outTime, String annotationHtml, Stimulus stimulus) {
         this.inTime = inTime;
         this.outTime = outTime;
         this.annotationHtml = annotationHtml;
+        this.stimulus = stimulus;
     }
 
     public double getInTime() {
@@ -46,5 +60,13 @@ public class AnnotationData {
 
     public void setAnnotationHtml(String annotationHtml) {
         this.annotationHtml = annotationHtml;
+    }
+
+    public Stimulus getStimulus() {
+        return stimulus;
+    }
+
+    public boolean intersectsTime(final double currentTime) {
+        return (currentTime >= inTime && currentTime <= outTime);
     }
 }
