cb8ba852627b98bcc4f8443766ce8ca9ada01f5f
Peter Withers <peter.withers@mpi.nl>
2015-12-09 16:51:28 +0100

    Started creating a cordova plugin to handle all audio recording and time aligned CSV tagging functions.

---
+++ FieldKitRecorder/src/android/nl/mpi/tg/eg/frinex/FieldKitRecorder.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2015 Max Planck Institute for Psycholinguistics, Nijmegen
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package nl.mpi.tg.eg.annotator;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+/**
+ * @since Dec 9, 2015 4:41:18 PM (creation date)
+ * @author Peter Withers <peter.withers@mpi.nl>
+ */
+public class FieldKitRecorder extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("tag")) {
+            String tagValue = args.getString(0);
+            writeTag(tagValue, callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    private void writeTag(String tagValue, CallbackContext callbackContext) {
+        if (tagValue != null && tagValue.length() > 0) {
+            callbackContext.success(tagValue);
+        } else {
+            callbackContext.error("Tag not supplied");
+        }
+    }
+}
