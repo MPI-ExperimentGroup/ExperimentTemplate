c0573187c188a4a07dadf1feaa772e3abf255228
Peter Withers <peter.withers@mpi.nl>
2016-01-19 18:37:01 +0100

    Added SynQuiz2 stimuli.

--- ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/util/SynQuiz2.java
+++ ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/util/SynQuiz2.java
@@ -17,6 +17,9 @@
  */
 package nl.mpi.tg.eg.experimentdesigner.util;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import nl.mpi.tg.eg.experimentdesigner.dao.PresenterFeatureRepository;
 import nl.mpi.tg.eg.experimentdesigner.dao.PresenterScreenRepository;
@@ -25,6 +28,7 @@ import nl.mpi.tg.eg.experimentdesigner.model.FeatureType;
 import nl.mpi.tg.eg.experimentdesigner.model.PresenterFeature;
 import nl.mpi.tg.eg.experimentdesigner.model.PresenterScreen;
 import nl.mpi.tg.eg.experimentdesigner.model.PresenterType;
+import nl.mpi.tg.eg.experimentdesigner.model.Stimulus;
 
 /**
  * @since Jan 18, 2016 11:20:47 AM (creation date)
@@ -33,8 +37,10 @@ import nl.mpi.tg.eg.experimentdesigner.model.PresenterType;
 public class SynQuiz2 {
 
     public void create(PresenterScreenRepository presenterScreenRepository, PresenterFeatureRepository presenterFeatureRepository, final List<PresenterScreen> presenterScreenList) {
-        PresenterScreen practiceScreen = createStimulusScreen("colourPicker");
-        presenterScreenList.add(practiceScreen);
+        presenterScreenList.add(createStimulusScreen("Weekdays"));
+        presenterScreenList.add(createStimulusScreen("Numbers"));
+        presenterScreenList.add(createStimulusScreen("Letters"));
+        presenterScreenList.add(createStimulusScreen("Months"));
     }
 
     private PresenterScreen createStimulusScreen(String screenName) {
@@ -46,15 +52,33 @@ public class SynQuiz2 {
         loadStimuliFeature.addFeatureAttributes(FeatureAttribute.randomise, "true");
         presenterFeatureList.add(loadStimuliFeature);
         final PresenterFeature hasMoreStimulusFeature = new PresenterFeature(FeatureType.hasMoreStimulus, null);
-        hasMoreStimulusFeature.getPresenterFeatureList().add(new PresenterFeature(FeatureType.stimulusLabel, null));
-        hasMoreStimulusFeature.getPresenterFeatureList().add(new PresenterFeature(FeatureType.showStimulusProgress, null));
+//        hasMoreStimulusFeature.getPresenterFeatureList().add(new PresenterFeature(FeatureType.stimulusLabel, null));
+//        hasMoreStimulusFeature.getPresenterFeatureList().add(new PresenterFeature(FeatureType.showStimulusProgress, null));
         loadStimuliFeature.getPresenterFeatureList().add(hasMoreStimulusFeature);
         final PresenterFeature endOfStimulusFeature = new PresenterFeature(FeatureType.endOfStimulus, null);
-        endOfStimulusFeature.getPresenterFeatureList().add(new PresenterFeature(FeatureType.text, "end of stimuli"));
-        final PresenterFeature menuButtonFeature = new PresenterFeature(FeatureType.targetButton, "Menu");
-        menuButtonFeature.addFeatureAttributes(FeatureAttribute.target, "AutoMenu");
-        endOfStimulusFeature.getPresenterFeatureList().add(menuButtonFeature);
+//        endOfStimulusFeature.getPresenterFeatureList().add(new PresenterFeature(FeatureType.text, "end of stimuli"));
+//        final PresenterFeature menuButtonFeature = new PresenterFeature(FeatureType.targetButton, "Menu");
+//        menuButtonFeature.addFeatureAttributes(FeatureAttribute.target, "AutoMenu");
+//        endOfStimulusFeature.getPresenterFeatureList().add(menuButtonFeature);
         loadStimuliFeature.getPresenterFeatureList().add(endOfStimulusFeature);
         return presenterScreen;
+    }
+
+    private void insertStimulusGroup(final ArrayList<Stimulus> stimuliList, String groupName, String groupItems) {
+        final HashSet<String> tagSet = new HashSet<>(Arrays.asList(new String[]{groupName}));
+        final String[] itemArray = groupItems.split(",");
+        for (String itemString : itemArray) {
+            final Stimulus stimulus = new Stimulus(null, null, null, null, itemString, itemString, 0, tagSet);
+            stimuliList.add(stimulus);
+        }
+    }
+
+    public ArrayList<Stimulus> createStimuli() {
+        final ArrayList<Stimulus> stimuliList = new ArrayList<>();
+        insertStimulusGroup(stimuliList, "Weekdays", "Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday");
+        insertStimulusGroup(stimuliList, "Numbers", "0,1,2,3,4,5,6,7,8,9");
+        insertStimulusGroup(stimuliList, "Letters", "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z");
+        insertStimulusGroup(stimuliList, "Months", "January,February,March,April,May,June,July,August,September,October,November,December");
+        return stimuliList;
     }
 }
