215150941653e5e13267032d1fbebf7b56e8072c
Peter Withers <peter.withers@mpi.nl>
2016-02-05 18:47:52 +0100

    Addressed the issue of corova ready and GWT loading such that directory based stimulus can be shown on app restart.

--- gwt-cordova/src/main/java/nl/mpi/tg/eg/experiment/client/ExperimentTemplate.java
+++ gwt-cordova/src/main/java/nl/mpi/tg/eg/experiment/client/ExperimentTemplate.java
@@ -13,7 +13,22 @@ public class ExperimentTemplate implements EntryPoint {
      * This is the entry point method.
      */
     @Override
-    public void onModuleLoad() {
+    public final native void onModuleLoad() /*-{
+        var self = this;
+        if ($wnd.isDeviceReady) {
+            self.@nl.mpi.tg.eg.experiment.client.ExperimentTemplate::onDeviceReady()();
+//            document.getElementById("widgetTag").className = device.platform; // todo: set the platform style
+        } else {
+            var listener = $entry(function () {
+                $doc.removeEventListener("deviceready", listener, false);
+                self.@nl.mpi.tg.eg.experiment.client.ExperimentTemplate::onDeviceReady()();
+//            document.getElementById("widgetTag").className = device.platform; // todo: set the platform style
+            });
+            $doc.addEventListener("deviceready", listener, false);
+         }
+     }-*/;
+
+    public void onDeviceReady() {
         final RootPanel prerunMessage = RootPanel.get("prerunMessage");
         if (prerunMessage != null) {
             prerunMessage.getElement().removeFromParent();
