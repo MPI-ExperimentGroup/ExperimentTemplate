af57e59d12275472d2208f192a18b41c89a0027e
Peter Withers <peter.withers@mpi.nl>
2016-02-12 16:55:21 +0100

    Added new texts to the SynQuiz2 test data.

--- ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/util/SynQuiz2.java
+++ ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/util/SynQuiz2.java
@@ -24,6 +24,12 @@ import java.util.List;
 import nl.mpi.tg.eg.experimentdesigner.dao.PresenterFeatureRepository;
 import nl.mpi.tg.eg.experimentdesigner.dao.PresenterScreenRepository;
 import nl.mpi.tg.eg.experimentdesigner.model.FeatureAttribute;
+import static nl.mpi.tg.eg.experimentdesigner.model.FeatureAttribute.link;
+import static nl.mpi.tg.eg.experimentdesigner.model.FeatureAttribute.maxHeight;
+import static nl.mpi.tg.eg.experimentdesigner.model.FeatureAttribute.maxWidth;
+import static nl.mpi.tg.eg.experimentdesigner.model.FeatureAttribute.percentOfPage;
+import static nl.mpi.tg.eg.experimentdesigner.model.FeatureAttribute.src;
+import static nl.mpi.tg.eg.experimentdesigner.model.FeatureAttribute.target;
 import nl.mpi.tg.eg.experimentdesigner.model.FeatureType;
 import nl.mpi.tg.eg.experimentdesigner.model.PresenterFeature;
 import nl.mpi.tg.eg.experimentdesigner.model.PresenterScreen;
@@ -36,11 +42,69 @@ import nl.mpi.tg.eg.experimentdesigner.model.Stimulus;
  */
 public class SynQuiz2 {
 
+    private final String imageSize = "80";
+
     public void create(PresenterScreenRepository presenterScreenRepository, PresenterFeatureRepository presenterFeatureRepository, final List<PresenterScreen> presenterScreenList) {
+        presenterScreenList.add(createIntroductionScreen("Introduction"));
+        presenterScreenList.add(createRegistrationScreen("Registration"));
+        presenterScreenList.add(createInformationScreen("Information"));
         presenterScreenList.add(createStimulusScreen("Weekdays"));
         presenterScreenList.add(createStimulusScreen("Numbers"));
         presenterScreenList.add(createStimulusScreen("Letters"));
         presenterScreenList.add(createStimulusScreen("Months"));
+    }
+
+    private PresenterScreen createIntroductionScreen(String screenName) {
+        final PresenterScreen presenterScreen = new PresenterScreen(screenName, screenName, null, screenName + "Screen", null, PresenterType.text);
+        List<PresenterFeature> presenterFeatureList = presenterScreen.getPresenterFeatureList();
+        presenterFeatureList.add(new PresenterFeature(FeatureType.centrePage, null));
+        presenterFeatureList.add(new PresenterFeature(FeatureType.text, "Decoding the Genetics of Synaesthesia"));
+        presenterFeatureList.add(new PresenterFeature(FeatureType.text, "We are studying the genetic basis of synaesthesia, a neurological phenomenon described as a \"mixing of the senses\". To find out how our genes shape how we see the world, we are looking for people who connect letters, numbers, days of the week, or months with specific colours. This is called \"grapheme-colour\" synaesthesia. "));
+        presenterFeatureList.add(new PresenterFeature(FeatureType.text, "How our study works:"));
+        final PresenterFeature studyDiagramFeature = new PresenterFeature(FeatureType.image, null);
+        studyDiagramFeature.addFeatureAttributes(percentOfPage, imageSize);
+        studyDiagramFeature.addFeatureAttributes(maxHeight, imageSize);
+        studyDiagramFeature.addFeatureAttributes(maxWidth, imageSize);
+        studyDiagramFeature.addFeatureAttributes(src, "StudyDiagram.svg");
+        studyDiagramFeature.addFeatureAttributes(link, "");
+        presenterFeatureList.add(studyDiagramFeature);
+        presenterFeatureList.add(new PresenterFeature(FeatureType.text, "The synaesthesia tests take about 20 minutes to complete, and you can choose the ones that apply to you. Depending on your scores, we may send you an email inviting you to participate in the genetics part of the study. There is no cost to participate, and you can do everything from home."));
+        final PresenterFeature targetButtonFeature = new PresenterFeature(FeatureType.targetButton, "Participate!");
+        targetButtonFeature.addFeatureAttributes(target, "registrationScreen");
+        presenterFeatureList.add(targetButtonFeature);
+        presenterFeatureList.add(new PresenterFeature(FeatureType.text, "For more information about synaesthesia, please see our 'About synaesthesia' page. If you are not sure if you have synaesthesia, and want to find out, try our SynQuiz app or take a quick test at synesthete.org."));
+        presenterFeatureList.add(new PresenterFeature(FeatureType.text, "This project is organised and funded by the Language & Genetics Department at the Max Planck Institute for Psycholinguistics in Nijmegen in the Netherlands, directed by Prof. Dr. Simon E. Fisher. The synaesthesia studies are coordinated by Dr. Amanda Tilot and Dr. Sarah Graham. If you have any questions about our research, please contact us at "+formatLink("synaesthesia@mpi.nl")+"."));
+        return presenterScreen;
+    }
+
+    private String formatLink(String linkUrl) {
+        return "<a href=\"#\" onclick=\"window.open(''" + linkUrl + "'',''_system''); return false;\">" + linkUrl + "</a>";
+    }
+
+    private PresenterScreen createRegistrationScreen(String screenName) {
+        final PresenterScreen presenterScreen = new PresenterScreen(screenName, screenName, null, screenName + "Screen", null, PresenterType.text);
+        List<PresenterFeature> presenterFeatureList = presenterScreen.getPresenterFeatureList();
+        presenterFeatureList.add(new PresenterFeature(FeatureType.centrePage, null));
+        presenterFeatureList.add(new PresenterFeature(FeatureType.text, "Please read the Participant Information Sheet carefully!"));
+        presenterFeatureList.add(new PresenterFeature(FeatureType.text, ""));
+        presenterFeatureList.add(new PresenterFeature(FeatureType.text, ""));
+        final PresenterFeature targetButtonFeature = new PresenterFeature(FeatureType.targetButton, "Participant Information Sheet");
+        targetButtonFeature.addFeatureAttributes(target, "informationScreen");
+        presenterFeatureList.add(targetButtonFeature);
+        return presenterScreen;
+    }
+
+    private PresenterScreen createInformationScreen(String screenName) {
+        final PresenterScreen presenterScreen = new PresenterScreen(screenName, screenName, null, screenName + "Screen", null, PresenterType.text);
+        List<PresenterFeature> presenterFeatureList = presenterScreen.getPresenterFeatureList();
+//        presenterFeatureList.add(new PresenterFeature(FeatureType.centrePage, null));
+//        presenterFeatureList.add(new PresenterFeature(FeatureType.text, "Please read the Participant Information Sheet carefully!"));
+//        presenterFeatureList.add(new PresenterFeature(FeatureType.text, ""));
+//        presenterFeatureList.add(new PresenterFeature(FeatureType.text, ""));
+//        final PresenterFeature targetButtonFeature = new PresenterFeature(FeatureType.targetButton, null);
+//        targetButtonFeature.addFeatureAttributes(target, "informationScreen");
+//        presenterFeatureList.add(targetButtonFeature);
+        return presenterScreen;
     }
 
     private PresenterScreen createStimulusScreen(String screenName) {
