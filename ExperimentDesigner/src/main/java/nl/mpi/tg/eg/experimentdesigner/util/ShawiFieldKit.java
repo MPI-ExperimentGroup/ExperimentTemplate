f3f41ce4ac1c974743da6b93d7e7eeed36f37bd5
Peter Withers <peter.withers@mpi.nl>
2016-03-01 15:29:32 +0100

    Added a presenter order by field so that the start screen and the order of menu items can be defined. Added more experiment configuration details.

--- ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/util/ShawiFieldKit.java
+++ ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/util/ShawiFieldKit.java
@@ -43,28 +43,33 @@ public class ShawiFieldKit {
         experiment.setPrimaryColour4("#feb24c");
         experiment.setPrimaryColour2("#f03b20");
         wizardController.addMetadata(experiment);
-        final PresenterScreen autoMenuPresenter = wizardController.addAutoMenu(experiment);
-        final PresenterScreen welcomePresenter = wizardController.addWelcomeScreen(experiment, autoMenuPresenter, null);
-        final PresenterScreen welcomeMenuPresenter = wizardController.addWelcomeMenu(experiment, welcomePresenter, null);
-        final PresenterScreen instructionsPresenter = wizardController.addInstructionsScreen(experiment, welcomePresenter, null);
+        final PresenterScreen autoMenuPresenter = wizardController.addAutoMenu(experiment, 18);
+        final PresenterScreen welcomePresenter = wizardController.addWelcomeScreen(experiment, autoMenuPresenter, null, 1);
+        final PresenterScreen welcomeMenuPresenter = wizardController.addWelcomeMenu(experiment, welcomePresenter, null, 2);
+        final PresenterScreen instructionsPresenter = wizardController.addInstructionsScreen(experiment, welcomePresenter, null, 3);
 
         StimuliSubAction[] picturesValuesArray = new StimuliSubAction[]{new StimuliSubAction("80", "the informant talks/says whatever s/he wants", "next")};
         StimuliSubAction[] grammaticalityValuesArray = new StimuliSubAction[]{new StimuliSubAction("80", new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})};
 // this should not be random but use alphanum sorting 
-        final PresenterScreen grammaticalityScreen = wizardController.createStimulusScreen(experiment, welcomePresenter, welcomePresenter, new String[]{"Grammaticality"}, grammaticalityValuesArray);
-        final PresenterScreen picturesScreen = wizardController.createStimulusScreen(experiment, welcomePresenter, grammaticalityScreen, new String[]{"Pictures"}, picturesValuesArray);
-        final PresenterScreen animalsScreen = wizardController.createStimulusScreen(experiment, welcomePresenter, grammaticalityScreen, new String[]{"Animals"}, picturesValuesArray);
-        final PresenterScreen frogsScreen = wizardController.createStimulusScreen(experiment, welcomePresenter, grammaticalityScreen, new String[]{"Frogs"}, picturesValuesArray);
-        final PresenterScreen metadataScreen = wizardController.createMetadataScreen(experiment, autoMenuPresenter, picturesScreen, new String[]{"Nombre", "Sexo", "Edad", "Estado civil", "Origen", "Lugar de residencia", "Nombre de la comunidad a la que pertenece", "Actividad laboral", "Nivel de estudios", "Número de hijos", "Religión", "Idiomas"});
-        final PresenterScreen selectUserPresenter = wizardController.addUserSelectMenu(experiment, welcomePresenter, metadataScreen);
-        final PresenterScreen editUserPresenter = wizardController.addEditUserScreen(experiment, welcomePresenter, metadataScreen);
-        final PresenterScreen debugScreenPresenter = wizardController.addDebugScreen(experiment, autoMenuPresenter);
-        final PresenterScreen kinshipPresenter = addKinshipScreen(experiment, autoMenuPresenter, null);
+        final PresenterScreen cutbreakScreen = wizardController.createStimulusScreen(experiment, welcomePresenter, welcomePresenter, new String[]{"cutbreak"}, grammaticalityValuesArray, false, 15);
+        final PresenterScreen grammarScreen = wizardController.createStimulusScreen(experiment, welcomePresenter, welcomePresenter, new String[]{"grammar"}, grammaticalityValuesArray, false, 14);
+        final PresenterScreen vanuatuScreen = wizardController.createStimulusScreen(experiment, welcomePresenter, welcomePresenter, new String[]{"vanuatu"}, grammaticalityValuesArray, false, 13);
+        final PresenterScreen bodiesScreen = wizardController.createStimulusScreen(experiment, welcomePresenter, welcomePresenter, new String[]{"bodies"}, grammaticalityValuesArray, false, 12);
+        final PresenterScreen bowpedScreen = wizardController.createStimulusScreen(experiment, welcomePresenter, welcomePresenter, new String[]{"bowped"}, grammaticalityValuesArray, false, 11);
+        final PresenterScreen grammaticalityScreen = wizardController.createStimulusScreen(experiment, welcomePresenter, welcomePresenter, new String[]{"Grammaticality"}, grammaticalityValuesArray, false, 7);
+        final PresenterScreen picturesScreen = wizardController.createStimulusScreen(experiment, welcomePresenter, grammaticalityScreen, new String[]{"Pictures"}, picturesValuesArray, false, 8);
+        final PresenterScreen animalsScreen = wizardController.createStimulusScreen(experiment, welcomePresenter, grammaticalityScreen, new String[]{"Animals"}, picturesValuesArray, false, 9);
+        final PresenterScreen frogsScreen = wizardController.createStimulusScreen(experiment, welcomePresenter, grammaticalityScreen, new String[]{"Frogs"}, picturesValuesArray, false, 10);
+        final PresenterScreen metadataScreen = wizardController.createMetadataScreen(experiment, autoMenuPresenter, picturesScreen, new String[]{"Nombre", "Sexo", "Edad", "Estado civil", "Origen", "Lugar de residencia", "Nombre de la comunidad a la que pertenece", "Actividad laboral", "Nivel de estudios", "Número de hijos", "Religión", "Idiomas"}, 4);
+        final PresenterScreen selectUserPresenter = wizardController.addUserSelectMenu(experiment, welcomePresenter, metadataScreen, 5);
+        final PresenterScreen editUserPresenter = wizardController.addEditUserScreen(experiment, welcomePresenter, metadataScreen, 6);
+        final PresenterScreen debugScreenPresenter = wizardController.addDebugScreen(experiment, autoMenuPresenter, 17);
+        final PresenterScreen kinshipPresenter = addKinshipScreen(experiment, autoMenuPresenter, null, 16);
         return experiment;
     }
 
-    public PresenterScreen addKinshipScreen(final Experiment experiment, final PresenterScreen backPresenter, final PresenterScreen nextPresenter) {
-        final PresenterScreen presenterScreen = new PresenterScreen("Kinship", "Kinship", backPresenter, "Kinship", nextPresenter, PresenterType.kindiagram);
+    public PresenterScreen addKinshipScreen(final Experiment experiment, final PresenterScreen backPresenter, final PresenterScreen nextPresenter, long displayOrder) {
+        final PresenterScreen presenterScreen = new PresenterScreen("Kinship", "Kinship", backPresenter, "Kinship", nextPresenter, PresenterType.kindiagram, displayOrder);
         final PresenterFeature presenterFeature1 = new PresenterFeature(FeatureType.addKinTypeGui, null);
         presenterFeature1.addFeatureAttributes(FeatureAttribute.diagramName, "kinDiagram");
         presenterScreen.getPresenterFeatureList().add(presenterFeature1);
