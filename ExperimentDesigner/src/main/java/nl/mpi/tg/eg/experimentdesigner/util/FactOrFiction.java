ce52c54761b9cf9f630ecad24cea424ff961a17e
Peter Withers <peter.withers@mpi.nl>
2016-03-21 16:34:30 +0100

    Added more details to the factorfiction experiment.

--- ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/util/FactOrFiction.java
+++ ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/util/FactOrFiction.java
@@ -20,6 +20,7 @@ package nl.mpi.tg.eg.experimentdesigner.util;
 import nl.mpi.tg.eg.experimentdesigner.controller.WizardController;
 import nl.mpi.tg.eg.experimentdesigner.model.Experiment;
 import nl.mpi.tg.eg.experimentdesigner.model.PresenterScreen;
+import nl.mpi.tg.eg.experimentdesigner.model.WizardData;
 
 /**
  * @since Mar 16, 2016 2:35:56 PM (creation date)
@@ -29,11 +30,24 @@ public class FactOrFiction {
 
     private final WizardController wizardController = new WizardController();
 
+    String[] attentionStimuli = new String[]{
+    };
+    String[] transportationStimuli = new String[]{
+        };
+
+    String multipleChoiceText = "1= niet zo graag, 7 = heel graag (same as survey 1 and 3)";
+    String[] readingBehavior = new String[]{};
+
     public Experiment getExperiment() {
         final Experiment experiment = wizardController.getExperiment("FactOrFiction", "Fact or Fiction");
-        final PresenterScreen editUserScreen = wizardController.addEditUserScreen(experiment, null, null, 2);
-        wizardController.addAgreementScreen(experiment, null, editUserScreen, 1, "Information about study & agreeing to participate");
-        wizardController.addRandomTextScreen(experiment, null, null, 2, "GroupAorB", new String[]{"IntroductionA:Will get some text", "IntroductionB:Will also get some text"});
+        final WizardData wizardData = new WizardData();
+        wizardData.setAgeField(true);
+        wizardData.setGenderField(true);
+        wizardData.setCustomTextField("level of proficiency in Dutch");
+        final PresenterScreen agreementScreen = wizardController.addAgreementScreen(experiment, null, "EditUser", 1, "Information about study & agreeing to participate");
+        final PresenterScreen randomTextScreen = wizardController.addRandomTextScreen(experiment, null, agreementScreen, 2, "GroupAorB", new String[]{"IntroductionA:Will get some text", "IntroductionB:Will also get some text"});
+        final PresenterScreen editUserScreen = wizardController.addEditUserScreen(experiment, agreementScreen, randomTextScreen, 2, wizardData);
+        randomTextScreen.setBackPresenter(editUserScreen);
         return experiment;
     }
 }
