e09ccbc71da9c21be0af598fda7d976b9814e439
Peter Withers <peter.withers@mpi.nl>
2016-04-07 17:30:40 +0200

    Added more parameters such as button texts to the wizard methods.

--- ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/util/SynQuiz2.java
+++ ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/util/SynQuiz2.java
@@ -69,7 +69,7 @@ public class SynQuiz2 {
 
         final PresenterScreen demographicsScreen = createDemographicsScreen(experiment, "Demographics", 3);
         presenterScreenList.add(demographicsScreen);
-        final PresenterScreen editUserScreen = wizardController.addEditUserScreen(experiment, introductionScreen, demographicsScreen, 2, wizardData, null, false);
+        final PresenterScreen editUserScreen = wizardController.addEditUserScreen(experiment, introductionScreen, "Edit User", demographicsScreen, 2, wizardData, null, null, "Continue", null, null, null, false);
         demographicsScreen.setBackPresenter(editUserScreen);
         final PresenterScreen weekdaysScreen = createStimulusScreen("Weekdays", demographicsScreen, 4);
         presenterScreenList.add(weekdaysScreen);
