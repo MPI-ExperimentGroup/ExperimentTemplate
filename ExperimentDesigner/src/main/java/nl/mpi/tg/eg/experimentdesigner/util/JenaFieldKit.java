17b0a0a355b9b1ce167ae03b6032aebbb895f23e
Peter Withers <peter.withers@mpi.nl>
2016-04-22 17:07:02 +0200

    Added more Bislama translations and added more flexibility to the current wizard to allow more UI translations in the short term.

--- ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/util/JenaFieldKit.java
+++ ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/util/JenaFieldKit.java
@@ -39,28 +39,28 @@ public class JenaFieldKit {
         final PresenterScreen welcomeMenuPresenter = wizardController.addWelcomeMenu(experiment, welcomePresenter, "Start", null, 2, "Niu rikording", "Gobak long wan olfala rikoding", "Makem wan niufala rikoding", "Gobak long wan rikoding we yu stat hem finis", false);
         final PresenterScreen instructionsPresenter = wizardController.addInstructionsScreen(experiment, welcomePresenter, "Instruksen", welcomeMenuPresenter, 3, "Wetem aplikasen ia yu save makem rikoding blong lanwis blong yu,"
                 + " bambai ol pipol blong Vanuatu mo ol pipol blong evri ples long world save harem lanwis blong yu. I gat fulap foto blong difren ples long Malakula wea i stap insaed long aplikasen ia. "
-                + "Bai yu showem ol foto ia long wan olfala woman o wan olfala man blong vilej blong yu mo askem long hem se i tokabaot ol foto ia long lanwis blong hem. Yu save transletem wanem i talem, tu.", false);
-        StimuliSubAction[] featureValuesArray = new StimuliSubAction[]{new StimuliSubAction("80", "speak the name in the language (lanwis)", "done"),
-            new StimuliSubAction("60", "It''s your turn! What did they say? Translate it into Bislama if you can.", "done"),
-            new StimuliSubAction("80", "ask for personal experience with... in language (lanwis)", "done"),
-            new StimuliSubAction("60", "It''s your turn! What experience did they have? Translate it into Bislama if you can.", "done"),
-            new StimuliSubAction("80", "custom story relating to ... (lanwis)", "done"),
-            new StimuliSubAction("60", "It''s your turn! What story did they tell? Translate it into Bislama if you can.", "done")
+                + "Bai yu showem ol foto ia long wan olfala woman o wan olfala man blong vilej blong yu mo askem long hem se i tokabaot ol foto ia long lanwis blong hem. Yu save transletem wanem i talem, tu.", "Go long program nao", false);
+        StimuliSubAction[] featureValuesArray = new StimuliSubAction[]{new StimuliSubAction("80", "Askem long man o woman wea i toktok se i talem nem blong wanem i stap lo foto long lanwis blong hem.", "Finis"),
+            new StimuliSubAction("60", "Noaia yu toktok. I talem wanem? Givim wan translesen long Bislama o Inglis o Franis.", "Finis"),
+            new StimuliSubAction("80", "Askem long man o woman we i toktok se i gat sam stori blong laef blong hem long saed blong ting ia we i stap long foto ia.", "Finis"),
+            new StimuliSubAction("60", "Noaia yu toktok. I talem wanem? Givim wan translesen long Bislama o Inglis o Franis.", "Finis"),
+            new StimuliSubAction("80", "Askem long man o woman we i toktok se i save wan kastom stori abaot ting ia we i stap long foto ia.", "Finis"),
+            new StimuliSubAction("60", "Noaia yu toktok. I talem wanem? Givim wan translesen long Bislama o Inglis o Franis.", "Finis")
         };
 
-        final PresenterScreen stimulusScreen = wizardController.createStimulusScreen(experiment, welcomePresenter, welcomePresenter, new String[]{"Pictures"}, featureValuesArray, true, 1000, true, 8, false);
+        final PresenterScreen stimulusScreen = wizardController.createStimulusScreen(experiment, welcomePresenter, "Lukluk ol foto", welcomePresenter, new String[]{"Pictures"}, featureValuesArray, true, 1000, true, "Finis olgeta", 8, false);
 //        final PresenterScreen vanuatuScreen = wizardController.createStimulusScreen(experiment, welcomePresenter, stimulusScreen, new String[]{"vanuatu"}, featureValuesArray, true, 1000, true, 7, false);
 //        final PresenterScreen bowpedStimulusScreen = wizardController.createStimulusScreen(experiment, welcomePresenter, vanuatuScreen, new String[]{"bowped"}, featureValuesArray, true, 1000, true, 9, false);
 //        final PresenterScreen bodiesStimulusScreen = wizardController.createStimulusScreen(experiment, welcomePresenter, bowpedStimulusScreen, new String[]{"bodies"}, featureValuesArray, true, 1000, true, 10, false);
         final PresenterScreen metadataScreen = wizardController.createMetadataScreen(experiment, welcomePresenter, stimulusScreen, new String[]{
-            "I stap rikod nao. Man o woman we i toktok bai i talem nem blong hem.", 
+            "I stap rikod nao. Man o woman we i toktok bai i talem nem blong hem.",
             "Bai i talem nem blong lanwis wea it toktok long hem.",
-            "Bai i talem nem blong ples wea i stap nao.", 
-            "Bai i talem nem blong ples wea i bon long hem.", 
+            "Bai i talem nem blong ples wea i stap nao.",
+            "Bai i talem nem blong ples wea i bon long hem.",
             "Bai i talem wanem yea i bon."
-        },"Neks","end of stimuli", 6, false);
+        }, "Neks", "Finis olgeta", 6, false);
         final PresenterScreen selectUserPresenter = wizardController.addUserSelectMenu(experiment, welcomePresenter, metadataScreen, 5, false);
-        final PresenterScreen editUserPresenter = wizardController.addEditUserScreen(experiment, welcomePresenter, "Edit User", metadataScreen, 4, "Could not contact the server, please check your internet connection and try again.", false);
+        final PresenterScreen editUserPresenter = wizardController.addEditUserScreen(experiment, welcomePresenter, "Infomesen blong man/woman we i toktok", "Edit User", metadataScreen, 4, null, null, new String[]{"speakerName:Nem blong man/woman we i toktok:.'{'3,'}':Please enter at least three letters."}, "Savem infomesen", null, null, null, false, "Could not contact the server, please check your internet connection and try again.", false);
         final PresenterScreen debugScreenPresenter = wizardController.addDebugScreen(experiment, autoMenuPresenter, 11, false);
         welcomeMenuPresenter.setNextPresenter(editUserPresenter);
 //        instructionsPresenter.setNextPresenter(metadataScreen);
