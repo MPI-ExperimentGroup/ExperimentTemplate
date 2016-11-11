0863269634915bf1006b5ec6570e6630d0363375
Peter Withers <peter.withers@mpi.nl>
2016-11-11 15:15:37 +0100

    Added more detail to the group participant service.

--- gwt-cordova/src/main/java/nl/mpi/tg/eg/experiment/client/service/GroupParticipantService.java
+++ gwt-cordova/src/main/java/nl/mpi/tg/eg/experiment/client/service/GroupParticipantService.java
@@ -17,6 +17,7 @@
  */
 package nl.mpi.tg.eg.experiment.client.service;
 
+import java.util.HashMap;
 import nl.mpi.tg.eg.experiment.client.listener.TimedStimulusListener;
 
 /**
@@ -25,14 +26,32 @@ import nl.mpi.tg.eg.experiment.client.listener.TimedStimulusListener;
  */
 public class GroupParticipantService implements TimedStimulusListener {
 
-    final String groupMembers;
-    final String groupCommunicationChannels;
-    final TimedStimulusListener connectedListener;
+    private final HashMap<String, TimedStimulusListener> groupActivityListeners = new HashMap<>();
+    private final String groupMembers;
+    private final String groupCommunicationChannels;
+    private final TimedStimulusListener connectedListener;
+    private final TimedStimulusListener messageListener;
+    private boolean isConnected = false;
 
-    public GroupParticipantService(String groupMembers, String groupCommunicationChannels, TimedStimulusListener connectedListener) {
+    private final String userId = null;
+    private String userLabel = null;
+    private String allMemberCodes = null;
+    private String memberCode = null;
+    private String groupId = null;
+    private String stimulusId = null;
+    private String messageString = null;
+    private Boolean groupReady = false;
+    private boolean userIdMatches = false;
+
+    public GroupParticipantService(String groupMembers, String groupCommunicationChannels, TimedStimulusListener connectedListener, TimedStimulusListener messageListener) {
         this.groupMembers = groupMembers;
         this.groupCommunicationChannels = groupCommunicationChannels;
         this.connectedListener = connectedListener;
+        this.messageListener = messageListener;
+    }
+
+    public void addGroupActivity(final String groupRole, final TimedStimulusListener activityListener) {
+        groupActivityListeners.put(groupRole, activityListener);
     }
 
     @Override
@@ -40,8 +59,57 @@ public class GroupParticipantService implements TimedStimulusListener {
         connectedListener.postLoadTimerFired();
     }
 
-    protected void handleGroupMessage(String userId, String userLabel, String allMemberCodes, String memberCode, String stimulusId, String messageString, Boolean groupReady) {
+    protected void handleGroupMessage(String userId, String userLabel, String groupId, String allMemberCodes, String memberCode, String stimulusId, String messageString, Boolean groupReady) {
+        userIdMatches = this.userId.equals(userId);
+        this.userLabel = userLabel;
+        this.allMemberCodes = allMemberCodes;
+        this.memberCode = memberCode;
+        this.groupId = groupId;
+        this.stimulusId = stimulusId;
+        this.messageString = messageString;
+        this.groupReady = groupReady;
+        messageListener.postLoadTimerFired();
+    }
+
+    protected void setConnected(Boolean isConnected) {
+        this.isConnected = isConnected;
         connectedListener.postLoadTimerFired();
+    }
+
+    public boolean isConnected() {
+        return isConnected;
+    }
+
+    public boolean isUserIdMatches() {
+        return userIdMatches;
+    }
+
+    public String getUserLabel() {
+        return userLabel;
+    }
+
+    public String getAllMemberCodes() {
+        return allMemberCodes;
+    }
+
+    public String getMemberCode() {
+        return memberCode;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public String getStimulusId() {
+        return stimulusId;
+    }
+
+    public String getMessageString() {
+        return messageString;
+    }
+
+    public boolean isGroupReady() {
+        return groupReady;
     }
 
     public native void joinGroupNetwork(String groupServerUrl, String groupName) /*-{
@@ -50,14 +118,31 @@ public class GroupParticipantService implements TimedStimulusListener {
         
         var socket = new $wnd.SockJS(groupServerUrl + 'gs-guide-websocket');
         stompClient = $wnd.Stomp.over(socket);
-        stompClient.connect({}, function (frame) {
-            setConnected(true);
+        stompClient.connect(
+            {
+//            withCredentials: false,
+//            noCredentials : true
+            }, 
+            function (frame) {
+            groupParticipantService.@nl.mpi.tg.eg.experiment.client.service.GroupParticipantService::setConnected(Ljava/lang/Boolean;)(@java.lang.Boolean::TRUE);
             console.log('Connected: ' + frame);
             stompClient.subscribe('/shared/group', function (groupMessage) {
                 var contentData = JSON.parse(groupMessage.body);
                 console.log('contentData: ' + contentData);
-                groupParticipantService.@nl.mpi.tg.eg.experiment.client.service.GroupParticipantService::handleGroupMessage(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/Boolean;)(contentData.userId, contentData.userLabel, contentData.groupId, contentData.allMemberCodes, contentData.memberCode, contentData.stimulusId, contentData.messageString, contentData.groupReady);
+                groupParticipantService.@nl.mpi.tg.eg.experiment.client.service.GroupParticipantService::handleGroupMessage(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/Boolean;)(contentData.userId, contentData.userLabel, contentData.groupId, contentData.allMemberCodes, contentData.memberCode, contentData.stimulusId, contentData.messageString, contentData.groupReady == true);
             });
         });
      }-*/;
+
+    public native void messageGroup(String userId, String messageString) /*-{
+    stompClient.send("/app/group", {}, JSON.stringify({
+        'userId': userId,
+        'userLabel': null,
+        'allMemberCodes': 'A,B,C,D,E,F,G',
+        'memberCode': null,
+        'stimulusId': Math.floor((1 + Math.random()) * 0x10000),
+        'messageString': messageString,
+        'groupReady': null
+    }));
+    }-*/;
 }
