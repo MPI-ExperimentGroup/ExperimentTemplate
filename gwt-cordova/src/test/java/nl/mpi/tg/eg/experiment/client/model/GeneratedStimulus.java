387ebf59c229c7903f871f60f9bd3368ac2b850c
Peter Withers <peter.withers@mpi.nl>
2017-03-28 14:56:57 +0200

    Added optional correct responses to stimuli.

--- gwt-cordova/src/test/java/nl/mpi/tg/eg/experiment/client/model/GeneratedStimulus.java
+++ gwt-cordova/src/test/java/nl/mpi/tg/eg/experiment/client/model/GeneratedStimulus.java
@@ -722,6 +722,16 @@ public class GeneratedStimulus implements Stimulus {
     }
 
     @Override
+    public boolean hasCorrectResponses() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getCorrectResponses() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
     public int compareTo(Stimulus o) {
         return this.uniqueId.compareTo(o.getUniqueId());
     }
