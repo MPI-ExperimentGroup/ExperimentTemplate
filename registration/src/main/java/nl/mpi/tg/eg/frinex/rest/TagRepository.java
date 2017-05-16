af829b75ebfefa61bfc1b63818f5e9c36c0ddaed
Peter Withers <peter.withers@mpi.nl>
2017-05-16 18:05:51 +0200

    Added the screen name and updated the admin page output.

--- registration/src/main/java/nl/mpi/tg/eg/frinex/rest/TagRepository.java
+++ registration/src/main/java/nl/mpi/tg/eg/frinex/rest/TagRepository.java
@@ -31,13 +31,13 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(collectionResourceRel = "tagevents", path = "tagevents")
 public interface TagRepository extends PagingAndSortingRepository<TagData, Long> {
 
-    @Query("select distinct new TagData(userId, eventTag, tagValue, eventMs, tagDate) from TagData order by tagDate asc")
+    @Query("select distinct new TagData(userId, screenName, eventTag, tagValue, eventMs, tagDate) from TagData order by tagDate asc")
     List<TagData> findAllDistinctRecords();
 
-    @Query("select distinct new TagData(userId, eventTag, tagValue, eventMs, tagDate) from TagData where userId = :userId order by tagDate asc, eventTag desc")
+    @Query("select distinct new TagData(userId, screenName, eventTag, tagValue, eventMs, tagDate) from TagData where userId = :userId order by tagDate asc, eventTag desc")
     List<TagData> findDistinctUserIdEventTagTagValueEventMsTageDateByUserIdOrderByTagDateAsc(@Param("userId") String userId);
 
-    @Query("select distinct new TagData(userId, eventTag, tagValue, eventMs, tagDate) from TagData where userId = :userId and eventTag = :eventTag order by tagDate asc") 
+    @Query("select distinct new TagData(userId, screenName, eventTag, tagValue, eventMs, tagDate) from TagData where userId = :userId and eventTag = :eventTag order by tagDate asc") 
     List<TagData> findByUserIdAndEventTagOrderByTagDateAsc(@Param("userId") String userId, @Param("eventTag") String eventTag);
 
     int countDistinctTagDateByUserIdAndTagValue(@Param("userId") String userId, @Param("tagValue") String tagValue);
