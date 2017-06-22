fa54e362aba3c11812b08382946e7ac60b682089
Peter Withers <peter.withers@mpi.nl>
2017-06-22 16:21:40 +0200

    Modified the stimuli allocation storage field name to be configurable.

--- ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/model/wizard/WizardVideoAudioOptionStimulusScreen.java
+++ ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/model/wizard/WizardVideoAudioOptionStimulusScreen.java
@@ -200,7 +200,7 @@ public class WizardVideoAudioOptionStimulusScreen extends AbstractWizardScreen {
 
     @Override
     public String getScreenTextInfo(int index) {
-        return new String[]{"Next Stimulus Button Label (when Allow Free Text is enabled)", "Validation Regex (when Allow Free Text is enabled)", "Validation Challenge (when Allow Free Text is enabled)", "Allowed Char Codes (when Allow Free Text is enabled)", "Next Button HotKey (when Allow Free Text is enabled)", "Free Text Hotkey"}[index];
+        return new String[]{"Next Stimulus Button Label (when Allow Free Text is enabled)", "Validation Regex (when Allow Free Text is enabled)", "Validation Challenge (when Allow Free Text is enabled)", "Allowed Char Codes (when Allow Free Text is enabled)", "Next Button HotKey (when Allow Free Text is enabled)", "Free Text Hotkey", "Storage field used for stimuli allocation URL parameters"}[index];
     }
 
     @Override
