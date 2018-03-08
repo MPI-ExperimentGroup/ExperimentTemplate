bcbb1bd806bea00ace574c11462aa3af0c4219d2
Peter Withers <peter.withers@mpi.nl>
2018-03-08 17:18:28 +0100

    Added a wizard utils data objects for file based experiment configurations.

---
+++ ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/model/wizard/WizardUtilStimuliData.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2018 Max Planck Institute for Psycholinguistics, Nijmegen
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package nl.mpi.tg.eg.experimentdesigner.model.wizard;
+
+/**
+ * @since Mar 8, 2018 12:18:58 PM (creation date)
+ * @author Peter Withers <peter.withers@mpi.nl>
+ */
+public class WizardUtilStimuliData {
+
+    protected String stimuliName;
+    protected String[] stimuliArray;
+    protected String[] randomStimuliTags;
+
+    public String[] getStimuliArray() {
+        return stimuliArray;
+    }
+
+    public void setStimuliArray(String[] stimuliArray) {
+        this.stimuliArray = stimuliArray;
+    }
+
+    public String[] getRandomStimuliTags() {
+        return randomStimuliTags;
+    }
+
+    public void setRandomStimuliTags(String[] randomStimuliTags) {
+        this.randomStimuliTags = randomStimuliTags;
+    }
+}
