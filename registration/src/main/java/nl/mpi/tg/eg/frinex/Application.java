3081a626318ced659913ca9da4c55be852813c36
Peter Withers <peter.withers@mpi.nl>
2018-03-27 16:11:00 +0200

    Replaced deprecated classes.

--- registration/src/main/java/nl/mpi/tg/eg/frinex/Application.java
+++ registration/src/main/java/nl/mpi/tg/eg/frinex/Application.java
@@ -20,7 +20,7 @@ package nl.mpi.tg.eg.frinex;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.hateoas.config.EnableEntityLinks;
