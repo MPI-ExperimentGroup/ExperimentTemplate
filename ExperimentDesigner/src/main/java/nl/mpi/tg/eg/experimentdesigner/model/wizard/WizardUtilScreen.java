ddd1ff882b220d8d564c8d114392c2aca5cf908c
Peter Withers <peter.withers@mpi.nl>
2018-04-23 15:05:25 +0200

    Updated the JSON configuration format to allow for multiple metadata screens.

---
+++ ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/model/wizard/WizardUtilScreen.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2018 Max Planck Institute for Psycholinguistics, Nijmegen
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package nl.mpi.tg.eg.experimentdesigner.model.wizard;
+
+/**
+ * @since Apr 23, 2018 11:22:34 AM (creation date)
+ * @author Peter Withers <peter.withers@mpi.nl>
+ */
+public class WizardUtilScreen {
+
+    protected WizardUtilStimuliData stimuliData;
+
+    protected String metadataText;
+    protected String[] metadataFields;
+
+    public WizardUtilStimuliData getStimuliData() {
+        return stimuliData;
+    }
+
+    public void setStimuliData(WizardUtilStimuliData stimuliData) {
+        this.stimuliData = stimuliData;
+    }
+
+    public String getMetadataText() {
+        return metadataText;
+    }
+
+    public void setMetadataText(String metadataText) {
+        this.metadataText = metadataText;
+    }
+
+    public String[] getMetadataFields() {
+        return metadataFields;
+    }
+
+    public void setMetadataFields(String[] metadataFields) {
+        this.metadataFields = metadataFields;
+    }
+}
