4586215060a40bcc56585c0742c63e57fb51f3a8
Peter Withers <peter.withers@mpi.nl>
2018-05-04 10:28:50 +0200

    Updated the JSON configuration format to allow for multiple screens of various sorts.

--- ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/model/wizard/WizardExistingUserCheckScreen.java
+++ ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/model/wizard/WizardExistingUserCheckScreen.java
@@ -17,7 +17,6 @@
  */
 package nl.mpi.tg.eg.experimentdesigner.model.wizard;
 
-import java.util.Arrays;
 import nl.mpi.tg.eg.experimentdesigner.model.Experiment;
 import nl.mpi.tg.eg.experimentdesigner.model.FeatureAttribute;
 import nl.mpi.tg.eg.experimentdesigner.model.FeatureType;
@@ -33,6 +32,7 @@ public class WizardExistingUserCheckScreen extends AbstractWizardScreen {
 
     public WizardExistingUserCheckScreen() {
         super(WizardScreenEnum.WizardExistingUserCheckScreen, "ExistingUserCheck", "ExistingUserCheck", "ExistingUserCheck");
+        setGenerateSelectUserMenuScreen(false);
     }
 
     public WizardExistingUserCheckScreen(final String screenTitle, final String new_Interview, final String resume_Interview, final String startNewText, final String resumeoldText) {
@@ -40,11 +40,12 @@ public class WizardExistingUserCheckScreen extends AbstractWizardScreen {
         this.wizardScreenData.setNextButton(new String[]{new_Interview, resume_Interview});
         this.wizardScreenData.setScreenText(0, startNewText);
         this.wizardScreenData.setScreenText(1, resumeoldText);
+        setGenerateSelectUserMenuScreen(false);
     }
 
     @Override
     public String getScreenBooleanInfo(int index) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return new String[]{"Generate Select User Menu Screen"}[index];
     }
 
     @Override
@@ -62,30 +63,44 @@ public class WizardExistingUserCheckScreen extends AbstractWizardScreen {
         return new String[]{}[index];
     }
 
+    final public void setGenerateSelectUserMenuScreen(boolean generateMenu) {
+        wizardScreenData.setScreenBoolean(0, generateMenu);
+    }
+
     @Override
     public PresenterScreen[] populatePresenterScreen(WizardScreenData storedWizardScreenData, Experiment experiment, boolean obfuscateScreenNames, long displayOrder) {
         super.populatePresenterScreen(storedWizardScreenData, experiment, obfuscateScreenNames, displayOrder);
         storedWizardScreenData.getPresenterScreen().setPresenterType(PresenterType.metadata);
+        storedWizardScreenData.getPresenterScreen().setNextPresenter(null);
         final PresenterFeature userCheckFeature = new PresenterFeature(FeatureType.existingUserCheck, null);
         final PresenterFeature multipleUsersFeature = new PresenterFeature(FeatureType.multipleUsers, null);
         userCheckFeature.getPresenterFeatureList().add(multipleUsersFeature);
         final PresenterFeature singleUserFeature = new PresenterFeature(FeatureType.singleUser, null);
         final PresenterFeature autoNextPresenter = new PresenterFeature(FeatureType.autoNextPresenter, null);
-        autoNextPresenter.addFeatureAttributes(FeatureAttribute.target, "Edit_User");
+        autoNextPresenter.addFeatureAttributes(FeatureAttribute.target, storedWizardScreenData.getNextWizardScreenData().getScreenTag());
         singleUserFeature.getPresenterFeatureList().add(autoNextPresenter);
         userCheckFeature.getPresenterFeatureList().add(singleUserFeature);
         multipleUsersFeature.getPresenterFeatureList().add(new PresenterFeature(FeatureType.plainText, storedWizardScreenData.getScreenText(0)));
         final PresenterFeature createUserFeature = new PresenterFeature(FeatureType.createUserButton, storedWizardScreenData.getNextButton()[0]);
-        createUserFeature.addFeatureAttributes(FeatureAttribute.target, "Edit_User");
+        createUserFeature.addFeatureAttributes(FeatureAttribute.target, storedWizardScreenData.getNextWizardScreenData().getScreenTag());
         multipleUsersFeature.getPresenterFeatureList().add(createUserFeature);
         multipleUsersFeature.getPresenterFeatureList().add(new PresenterFeature(FeatureType.addPadding, null));
         multipleUsersFeature.getPresenterFeatureList().add(new PresenterFeature(FeatureType.plainText, storedWizardScreenData.getScreenText(1)));
 
-        final PresenterFeature selectUserFeature = new PresenterFeature(FeatureType.targetButton, storedWizardScreenData.getNextButton()[1]);
-        selectUserFeature.addFeatureAttributes(FeatureAttribute.target, "SelectUser");
-        multipleUsersFeature.getPresenterFeatureList().add(selectUserFeature);
+        final PresenterFeature selectUserButton = new PresenterFeature(FeatureType.targetButton, storedWizardScreenData.getNextButton()[1]);
+        selectUserButton.addFeatureAttributes(FeatureAttribute.target, "SelectUser");
+        multipleUsersFeature.getPresenterFeatureList().add(selectUserButton);
         storedWizardScreenData.getPresenterScreen().getPresenterFeatureList().add(userCheckFeature);
         experiment.getPresenterScreen().add(storedWizardScreenData.getPresenterScreen());
-        return new PresenterScreen[]{storedWizardScreenData.getPresenterScreen()};
+
+        if (storedWizardScreenData.getScreenBoolean(0)) {
+            PresenterScreen selectUserScreen = new PresenterScreen(storedWizardScreenData.getScreenTitle(), storedWizardScreenData.getScreenTitle(), storedWizardScreenData.getPresenterScreen(), "SelectUser", storedWizardScreenData.getNextWizardScreenData().getPresenterScreen(), PresenterType.metadata, displayOrder + 1);
+            final PresenterFeature selectUserFeature = new PresenterFeature(FeatureType.selectUserMenu, null);
+            selectUserScreen.getPresenterFeatureList().add(selectUserFeature);
+            experiment.getPresenterScreen().add(selectUserScreen);
+            return new PresenterScreen[]{storedWizardScreenData.getPresenterScreen(), selectUserScreen};
+        } else {
+            return new PresenterScreen[]{storedWizardScreenData.getPresenterScreen()};
+        }
     }
 }
