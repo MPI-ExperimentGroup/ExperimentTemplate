6c570d69ca57f87666cade9bf1901bdbb17f826b
Peter Withers <peter.withers@mpi.nl>
2018-06-08 14:36:15 +0200

    Updated the electron configuration files based on wingcode.

--- gwt-cordova/src/main/electron/setup-electron.sh
+++ gwt-cordova/src/main/electron/setup-electron.sh
@@ -11,6 +11,9 @@ cd $appname-electron
 
 
 npm install -g electron-forge
-electron-forge init $appname
-cd $appname
-electron-forge make
+npm install
+#electron-forge init $appname
+#cd $appname
+#electron-forge make all
+electron-forge package --platform=win32 --arch=x64
+cp out/make/*darwin*.zip $appname.zip ../@experiment.configuration.name@-darwin.zip
