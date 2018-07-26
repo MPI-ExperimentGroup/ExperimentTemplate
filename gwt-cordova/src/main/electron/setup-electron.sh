25a00bfe6dfb0723a7e3b1850f752d5683fa2f85
Peter Withers <peter-gthb@bambooradical.com>
2018-07-26 11:56:30 +0200

    Updated the npm build script and its global paths.

--- gwt-cordova/src/main/electron/setup-electron.sh
+++ gwt-cordova/src/main/electron/setup-electron.sh
@@ -8,12 +8,16 @@ unzip $appname-electron.zip -d $appname-electron
 cd $appname-electron
 
 
-
-
+npm config set prefix '/srv/ExperimentTemplate/.npm-global'
+PATH=/srv/ExperimentTemplate/.npm-global/bin:$PATH
 npm install -g electron-forge
 npm install --save
 #electron-forge init $appname
 #cd $appname
 #electron-forge package --platform=win32 --arch=x64
 electron-forge make all
-cp out/make/*darwin*.zip $appname.zip ../@experiment.configuration.name@-darwin.zip
+
+mkdir /srv/target/electron
+cp out/make/*linux*.zip ../@experiment.configuration.name@-linux.zip
+cp out/make/*win32*.zip ../@experiment.configuration.name@-win32.zip
+cp out/make/*darwin*.zip ../@experiment.configuration.name@-darwin.zip
