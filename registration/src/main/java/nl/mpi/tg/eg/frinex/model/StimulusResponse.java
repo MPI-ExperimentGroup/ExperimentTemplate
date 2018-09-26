4aea84bac78d905f9549f992d2efcf2a8f5aaab2
Peter Withers <peter.withers@mpi.nl>
2018-09-26 16:25:19 +0200

    Added explicit collection of stimulus responses.

---
+++ registration/src/main/java/nl/mpi/tg/eg/frinex/model/StimulusResponse.java
@@ -0,0 +1,264 @@
+/*
+ * Copyright (C) 2018 Max Planck Institute for Psycholinguistics, Nijmegen
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package nl.mpi.tg.eg.frinex.model;
+
+import java.util.Date;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+
+/**
+ * @since Sep 26, 2018 2:15:01 PM (creation date)
+ * @author Peter Withers <peter.withers@mpi.nl>
+ */
+@Entity
+public class StimulusResponse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Date tagDate;
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Date submitDate;
+    private String experimentName;
+    private String screenName;
+    private Integer dataChannel;
+    private String stimulusId;
+    private String response;
+    private String correctness;
+    private String userId;
+    private int eventMs;
+
+    private int gamesPlayed;
+    private int totalScore;
+    private int totalPotentialScore;
+    private int currentScore;
+    private int correctStreak;
+    private int errorStreak;
+    private int potentialScore;
+    private double maxScore;
+    private int maxErrors;
+    private int maxCorrectStreak;
+    private int maxErrorStreak;
+    private int maxPotentialScore;
+
+    public long getId() {
+        return id;
+    }
+
+    public Date getTagDate() {
+        return tagDate;
+    }
+
+    public Date getSubmitDate() {
+        return submitDate;
+    }
+
+    public String getExperimentName() {
+        return experimentName;
+    }
+
+    public String getScreenName() {
+        return screenName;
+    }
+
+    public Integer getDataChannel() {
+        return dataChannel;
+    }
+
+    public String getStimulusId() {
+        return stimulusId;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public String getCorrectness() {
+        return correctness;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public int getEventMs() {
+        return eventMs;
+    }
+
+    public int getGamesPlayed() {
+        return gamesPlayed;
+    }
+
+    public int getTotalScore() {
+        return totalScore;
+    }
+
+    public int getTotalPotentialScore() {
+        return totalPotentialScore;
+    }
+
+    public int getCurrentScore() {
+        return currentScore;
+    }
+
+    public int getCorrectStreak() {
+        return correctStreak;
+    }
+
+    public int getErrorStreak() {
+        return errorStreak;
+    }
+
+    public int getPotentialScore() {
+        return potentialScore;
+    }
+
+    public double getMaxScore() {
+        return maxScore;
+    }
+
+    public int getMaxErrors() {
+        return maxErrors;
+    }
+
+    public int getMaxCorrectStreak() {
+        return maxCorrectStreak;
+    }
+
+    public int getMaxErrorStreak() {
+        return maxErrorStreak;
+    }
+
+    public int getMaxPotentialScore() {
+        return maxPotentialScore;
+    }
+
+    public void setSubmitDate(Date submitDate) {
+        this.submitDate = submitDate;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 47 * hash + Objects.hashCode(this.tagDate);
+        hash = 47 * hash + Objects.hashCode(this.experimentName);
+        hash = 47 * hash + Objects.hashCode(this.screenName);
+        hash = 47 * hash + Objects.hashCode(this.dataChannel);
+        hash = 47 * hash + Objects.hashCode(this.stimulusId);
+        hash = 47 * hash + Objects.hashCode(this.response);
+        hash = 47 * hash + Objects.hashCode(this.correctness);
+        hash = 47 * hash + Objects.hashCode(this.userId);
+        hash = 47 * hash + this.eventMs;
+        hash = 47 * hash + this.gamesPlayed;
+        hash = 47 * hash + this.totalScore;
+        hash = 47 * hash + this.totalPotentialScore;
+        hash = 47 * hash + this.currentScore;
+        hash = 47 * hash + this.correctStreak;
+        hash = 47 * hash + this.errorStreak;
+        hash = 47 * hash + this.potentialScore;
+        hash = 47 * hash + (int) (Double.doubleToLongBits(this.maxScore) ^ (Double.doubleToLongBits(this.maxScore) >>> 32));
+        hash = 47 * hash + this.maxErrors;
+        hash = 47 * hash + this.maxCorrectStreak;
+        hash = 47 * hash + this.maxErrorStreak;
+        hash = 47 * hash + this.maxPotentialScore;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final StimulusResponse other = (StimulusResponse) obj;
+        if (this.eventMs != other.eventMs) {
+            return false;
+        }
+        if (this.gamesPlayed != other.gamesPlayed) {
+            return false;
+        }
+        if (this.totalScore != other.totalScore) {
+            return false;
+        }
+        if (this.totalPotentialScore != other.totalPotentialScore) {
+            return false;
+        }
+        if (this.currentScore != other.currentScore) {
+            return false;
+        }
+        if (this.correctStreak != other.correctStreak) {
+            return false;
+        }
+        if (this.errorStreak != other.errorStreak) {
+            return false;
+        }
+        if (this.potentialScore != other.potentialScore) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.maxScore) != Double.doubleToLongBits(other.maxScore)) {
+            return false;
+        }
+        if (this.maxErrors != other.maxErrors) {
+            return false;
+        }
+        if (this.maxCorrectStreak != other.maxCorrectStreak) {
+            return false;
+        }
+        if (this.maxErrorStreak != other.maxErrorStreak) {
+            return false;
+        }
+        if (this.maxPotentialScore != other.maxPotentialScore) {
+            return false;
+        }
+        if (!Objects.equals(this.experimentName, other.experimentName)) {
+            return false;
+        }
+        if (!Objects.equals(this.screenName, other.screenName)) {
+            return false;
+        }
+        if (!Objects.equals(this.stimulusId, other.stimulusId)) {
+            return false;
+        }
+        if (!Objects.equals(this.response, other.response)) {
+            return false;
+        }
+        if (!Objects.equals(this.correctness, other.correctness)) {
+            return false;
+        }
+        if (!Objects.equals(this.userId, other.userId)) {
+            return false;
+        }
+        if (!Objects.equals(this.tagDate, other.tagDate)) {
+            return false;
+        }
+        if (!Objects.equals(this.dataChannel, other.dataChannel)) {
+            return false;
+        }
+        return true;
+    }
+}
