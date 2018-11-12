3f39891f0e4d362d8899d95416a577d81c8171ad
olhshk <olhshk@m17405324.mpi.nl>
2018-11-12 16:46:58 +0100

    making xml fragment stimuli sentence comprehension

--- AdaptiveVocabularyAssessmentModule/src/main/java/nl/mpi/tg/eg/frinex/adaptivevocabularyassessment/client/service/sentencecomprehensionpool/SentenceComprehensionPicturesStimuliFromString.java
+++ AdaptiveVocabularyAssessmentModule/src/main/java/nl/mpi/tg/eg/frinex/adaptivevocabularyassessment/client/service/sentencecomprehensionpool/SentenceComprehensionPicturesStimuliFromString.java
@@ -35,52 +35,79 @@ import nl.mpi.tg.eg.frinex.adaptivevocabularyassessment.client.generic.CsvRecord
 public class SentenceComprehensionPicturesStimuliFromString {
 
     //Trial,Item,Gender,Log_frequency,Syllables,Correct_response,Picture
-    public String parseTrialsPicturesStringIntoXml(String csvString, String stimuliDir) throws Exception {
+    public String parseTrialsPicturesStringIntoXml(String csvString, String baseDir, String pictureStimuliDir) throws Exception {
 
         StringBuilder builder = new StringBuilder();
 
-        CsvRecords csvWrapper = new CsvRecords(null, ",", "\n");
+        CsvRecords csvWrapper = new CsvRecords(null, ";", "\n");
         csvWrapper.readRecords(csvString);
         ArrayList<LinkedHashMap<String, String>> records = csvWrapper.getRecords();
 
         for (LinkedHashMap<String, String> record : records) {
 
-            String trialNumber = record.get("Trial").trim();
+            String trialNumber = record.get("Trial");
             if (trialNumber == null) {
                 throw new IOException("Trial is undefined");
+            } else {
+                trialNumber = trialNumber.trim();
             }
 
-            String item = record.get("Item").trim();
+            String item = record.get("Item");
             if (item == null) {
                 throw new IOException("Item is undefined");
+            } else {
+                item = item.trim();
             }
 
-            String logFrequency = record.get("Log_frequency").trim();
+            String logFrequency = record.get("Log_frequency");
             if (logFrequency == null) {
                 throw new IOException("Log_frequency is undefined");
+            } else {
+                logFrequency = logFrequency.trim();
             }
-            logFrequency = logFrequency.replaceAll("\\.", "");
+            logFrequency = logFrequency.replaceAll("\\,", "_comma_");
 
-            String syllables = record.get("Syllables").trim();
+            String syllables = record.get("Syllables");
             if (syllables == null) {
                 throw new IOException("Syllables is undefined");
+            } else {
+                syllables = syllables.trim();
             }
 
-            String correctResponce = record.get("Correct_response").trim();
-            if (correctResponce == null) {
+            String correctResponse = record.get("Correct_response");
+            if (correctResponse == null) {
                 throw new IOException("Correct_response is undefined");
+            }
+            if (correctResponse.trim().equals("left")) {
+                correctResponse = "Z";
+            } else {
+                if (correctResponse.trim().equals("right")) {
+                    correctResponse = "M";
+                } else {
+                    throw new IOException("Correct_response is defined incorrectly");
+                }
             }
 
             String picture = record.get("Picture").trim();
             if (picture == null) {
                 throw new IOException("Picture is undefined");
+            } else {
+                picture = picture.trim();
             }
-            
-            String imagePath = stimuliDir + picture;
-            String uniqueId = "part1_"+ trialNumber+"_"+item;
+
+            try {
+                //Path picturePath = Paths.get(originalDir + "pictures/" + picture);
+                BufferedReader br1 = new BufferedReader(new FileReader(baseDir + pictureStimuliDir + picture));
+                br1.close();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+            String imagePath = pictureStimuliDir + picture;
+            String uniqueId = "part1_" + trialNumber + "_" + item;
             String label = item;
             String tags = "part1  target_frequency_" + logFrequency + " syllables_" + syllables;
-            String currentSt = this.makePictureStimulusString(uniqueId, label, imagePath, tags);
+            String currentSt = this.makePictureStimulusString(uniqueId, label, correctResponse, imagePath, tags);
             builder.append(currentSt);
 
         }
@@ -90,6 +117,7 @@ public class SentenceComprehensionPicturesStimuliFromString {
 
     private String makePictureStimulusString(String uniqueId,
             String label,
+            String correctResponse,
             String imagePath,
             String tags) {
 
@@ -100,6 +128,7 @@ public class SentenceComprehensionPicturesStimuliFromString {
 
         retVal.append(" pauseMs=\"0\" ");
         retVal.append(" imagePath=\"").append(imagePath).append("\" ");
+        retVal.append(" correctResponses=\"").append(correctResponse).append("\" ");
 
         retVal.append(" tags=\"").append(tags).append("\" ");
 
@@ -108,166 +137,189 @@ public class SentenceComprehensionPicturesStimuliFromString {
 
     }
 
-    public String parseTrialsAudioPicturesStringIntoXml(String csvString, String originalDir, String stimuliDir) throws Exception {
+    //"Trial;Condition;Item;Gender;Log_frequency_target;Log_frequency_distractor;Syllables_target;Syllables_distractor;Picture_target;Picture_distractor;Position_target;Wav;determiner_onset;target_onset;Timer;Correct_response\n"
+    public String parseTrialsAudioPicturesStringIntoXml(String csvString, String pictureStimuliDir, String audioStimuliDir, String codeStimuliDir, String baseDir) throws Exception {
 
         StringBuilder builder = new StringBuilder();
 
-        CsvRecords csvWrapper = new CsvRecords(null, ",", "\n");
+        CsvRecords csvWrapper = new CsvRecords(null, ";", "\n");
         csvWrapper.readRecords(csvString);
         ArrayList<LinkedHashMap<String, String>> records = csvWrapper.getRecords();
 
         for (LinkedHashMap<String, String> record : records) {
 
             //Trial,Block,Item,Gender,Log_frequency_target,Log_frequency_distractor,
-            String trialNumber = record.get("Trial").trim();
+            String trialNumber = record.get("Trial");
             if (trialNumber == null) {
                 throw new IOException("Trial is undefined");
+            } else {
+                trialNumber = trialNumber.trim();
             }
 
-            String block = record.get("Block").trim();
-            if (block == null) {
-                throw new IOException("Block is undefined");
+            String condition = record.get("Condition");
+            if (condition == null) {
+                throw new IOException("Condition is undefined");
+            } else {
+                condition = condition.trim();
             }
+            condition =  condition.replaceAll("-", "_");
 
-            String item = record.get("Item").trim();
+
+            String item = record.get("Item");
             if (item == null) {
                 throw new IOException("Item is undefined");
+            } else {
+                item = item.trim();
             }
 
-            String gender = record.get("Gender").trim();
+            String gender = record.get("Gender");
             if (gender == null) {
-                throw new IOException("Item is undefined");
+                throw new IOException("Gender is undefined");
+            } else {
+                gender = gender.trim();
             }
 
-            String logFrequencyTarget = record.get("Log_frequency_target").trim();
+            String logFrequencyTarget = record.get("Log_frequency_target");
             if (logFrequencyTarget == null) {
                 throw new IOException("Log_frequency_target is undefined");
+            } else {
+                logFrequencyTarget = logFrequencyTarget.trim();
             }
-            logFrequencyTarget = logFrequencyTarget.replaceAll("\\.", "");
+            logFrequencyTarget = logFrequencyTarget.replaceAll("\\,", "_comma_");
 
-            String logFrequencyDistractor = record.get("Log_frequency_distractor").trim();
+            String logFrequencyDistractor = record.get("Log_frequency_distractor");
             if (logFrequencyDistractor == null) {
                 throw new IOException("Log_frequency_distractor is undefined");
+            } else {
+                logFrequencyDistractor = logFrequencyDistractor.trim();
             }
-            logFrequencyDistractor = logFrequencyDistractor.replaceAll("\\.", "");
+            logFrequencyDistractor = logFrequencyDistractor.replaceAll("\\,", "_comma_");
 
             //Syllables_target,Syllables_distractor,Picture_target,Picture_distractor,
-            String syllablesTarget = record.get("Syllables_target").trim();
+            String syllablesTarget = record.get("Syllables_target");
             if (syllablesTarget == null) {
                 throw new IOException("Syllables_target is undefined");
+            } else {
+                syllablesTarget = syllablesTarget.trim();
             }
-            String syllablesDistractor = record.get("Syllables_distractor").trim();
+
+            String syllablesDistractor = record.get("Syllables_distractor");
             if (syllablesDistractor == null) {
                 throw new IOException("Syllables_distractor is undefined");
+            } else {
+                syllablesDistractor = syllablesDistractor.trim();
             }
 
-            String pictureTarget = record.get("Picture_target").trim();
+            String pictureTarget = record.get("Picture_target");
             if (pictureTarget == null) {
                 throw new IOException("Picture_target is undefined");
+            } else {
+                pictureTarget = pictureTarget.trim();
             }
 
-            String pictureDistractor = record.get("Picture_distractor").trim();
+            String pictureDistractor = record.get("Picture_distractor");
             if (pictureDistractor == null) {
                 throw new IOException("Picture_distractor is undefined");
+            } else {
+                pictureDistractor = pictureDistractor.trim();
             }
 
             //Position_target,Wav,determiner_onset,target_onset,Timer,Correct_response
-            String positionTarget = record.get("Position_target").trim();
+            String positionTarget = record.get("Position_target");
             if (positionTarget == null) {
                 throw new IOException("Position_target is undefined");
+            } else {
+                positionTarget = positionTarget.trim();
             }
 
-            String correctResponse = record.get("Correct_response").trim();
+            String correctResponse = record.get("Correct_response");
             if (correctResponse == null) {
                 throw new IOException("Correct_response is undefined");
             }
+            if (correctResponse.trim().equals("left")) {
+                correctResponse = "Z";
+            } else {
+                if (correctResponse.trim().equals("right")) {
+                    correctResponse = "M";
+                } else {
+                    throw new IOException("Correct_response is defined incorrectly");
+                }
+            }
 
-            String audio = record.get("Wav").trim();
+            String audio = record.get("Wav");
             if (audio == null) {
                 throw new IOException("Wav is undefined");
+            } else {
+                audio = audio.trim();
             }
             if (audio.endsWith(".wav")) {
                 int length = audio.length();
-                audio = audio.substring(0, length-4);
+                audio = audio.substring(0, length - 4);
             }
 
-            String determinerOnset = record.get("determiner_onset").trim();
+            String determinerOnset = record.get("determiner_onset");
             if (determinerOnset == null) {
                 throw new IOException("determiner_onset is undefined");
+            } else {
+                determinerOnset = determinerOnset.trim();
             }
 
-            determinerOnset = determinerOnset.replaceAll("\\.", "");
+            determinerOnset = determinerOnset.replaceAll("\\,", "_comma_");
 
-            String targetOnset = record.get("target_onset").trim();
+            String targetOnset = record.get("target_onset");
             if (targetOnset == null) {
                 throw new IOException("target_onset is undefined");
+            } else {
+                targetOnset = targetOnset.trim();
             }
 
-            targetOnset = targetOnset.replaceAll("\\.", "");
+            targetOnset = targetOnset.replaceAll("\\,", "_comma_");
 
-            String timer = record.get("Timer").trim();
+            String timer = record.get("Timer");
             if (timer == null) {
                 throw new IOException("Timer is undefined");
+            } else {
+                timer = timer.trim();
             }
-            timer = timer.replaceAll("\\.", "");
+            timer = timer.replaceAll("\\,", "_comma_");
 
-            String uniqueId = "part2_" + trialNumber+"_"+item;
+            String uniqueId = "part2_" + trialNumber + "_" + item;
             // creating patternly-named copies of images 
-            Path sourceTarget = Paths.get(originalDir + "pictures/" + pictureTarget);
-            Path sourceDistractor = Paths.get(originalDir + "pictures/" + pictureDistractor);
-            Path destTarget = Paths.get(stimuliDir + uniqueId + "_target.png");
-            Path destDistractor = Paths.get(stimuliDir + uniqueId + "_distractor.png");
             try {
+                Path sourceTarget = Paths.get(baseDir + pictureStimuliDir + pictureTarget);
+                Path sourceDistractor = Paths.get(baseDir + pictureStimuliDir + pictureDistractor);
+                Path destTarget = Paths.get(baseDir + codeStimuliDir + uniqueId + "_target.png");
+                Path destDistractor = Paths.get(baseDir + codeStimuliDir + uniqueId + "_distractor.png");
+
                 Files.copy(sourceTarget, destTarget, StandardCopyOption.REPLACE_EXISTING);
                 Files.copy(sourceDistractor, destDistractor, StandardCopyOption.REPLACE_EXISTING);
                 // end creating copies
                 // sanity check
-                BufferedReader br1 = new BufferedReader(new FileReader(stimuliDir + uniqueId + "_target.png"));
+                BufferedReader br1 = new BufferedReader(new FileReader(baseDir + codeStimuliDir + uniqueId + "_target.png"));
                 br1.close();
-                BufferedReader br2 = new BufferedReader(new FileReader(stimuliDir + uniqueId + "_distractor.png"));
+                BufferedReader br2 = new BufferedReader(new FileReader(baseDir + codeStimuliDir + uniqueId + "_distractor.png"));
                 br2.close();
                 // end sanity check 
             } catch (Exception e) {
-                System.out.println(e);
-                return e.getMessage();
-            }
-            
-            // creating patternly-named copies of audio 
-            Path sourceTargetOgg = Paths.get(originalDir + "audiofiles/" + audio+".ogg");
-            Path sourceTargetMp3 = Paths.get(originalDir + "audiofiles/" + audio+".mp3");
-            Path destTargetOgg = Paths.get(stimuliDir + uniqueId + "_target.ogg");
-            Path destTargetMp3 = Paths.get(stimuliDir + uniqueId + "_target.mp3");
-            try {
-                Files.copy(sourceTargetOgg, destTargetOgg, StandardCopyOption.REPLACE_EXISTING);
-                Files.copy(sourceTargetMp3, destTargetMp3, StandardCopyOption.REPLACE_EXISTING);
-                // end creating copies
-                // sanity check
-                BufferedReader br1 = new BufferedReader(new FileReader(stimuliDir + uniqueId + "_target.ogg"));
-                br1.close();
-                BufferedReader br2 = new BufferedReader(new FileReader(stimuliDir + uniqueId + "_target.mp3"));
-                br2.close();
-                // end sanity check 
-            } catch (Exception e) {
-                System.out.println(e);
-                return e.getMessage();
+                System.out.println(e.getMessage());
             }
 
-            
-            
             String label = item;
-            
+
             //Gender,Log_frequency_target,Log_frequency_distractor,Syllables_target,Syllables_distractor,Position_target,determiner_onset,target_onset,Timer
-            String tags = "oart1 gender_"+gender + "  Log_frequency_target_" + logFrequencyTarget + 
-                    "  Log_frequency_distractor_" + logFrequencyDistractor +
-                    " Syllables_target_" + syllablesTarget + 
-                    " Syllables_distractor_" + syllablesDistractor +
-                    " Position_target_" + positionTarget + 
-                    " determiner_onset_" + determinerOnset + 
-                    " target_onset_" + targetOnset + 
-                     " timer_" + timer;
-            
-            
-            String currentSt = this.makeAudioPictureStimulusString(uniqueId, label, correctResponse, uniqueId,tags);
+            String tags = "part2 " + positionTarget + 
+                    " condition_" +condition +
+                    " gender_" + gender + "  Log_frequency_target_" + logFrequencyTarget
+                    + "  Log_frequency_distractor_" + logFrequencyDistractor
+                    + " Syllables_target_" + syllablesTarget
+                    + " Syllables_distractor_" + syllablesDistractor
+                    + " Position_target_" + positionTarget
+                    + " determiner_onset_" + determinerOnset
+                    + " target_onset_" + targetOnset
+                    + " timer_" + timer;
+
+            String audioPath = audioStimuliDir + audio;
+            String currentSt = this.makeAudioPictureStimulusString(uniqueId, label, correctResponse, audioPath, uniqueId, tags);
             builder.append(currentSt);
 
         }
@@ -278,6 +330,7 @@ public class SentenceComprehensionPicturesStimuliFromString {
     private String makeAudioPictureStimulusString(String uniqueId,
             String label,
             String correctResponse,
+            String audioPath,
             String code,
             String tags) {
 
@@ -287,7 +340,8 @@ public class SentenceComprehensionPicturesStimuliFromString {
         retVal.append(" label=\"").append(label).append("\" ");
 
         retVal.append(" pauseMs=\"0\" ");
-        
+
+        retVal.append(" audioPath=\"").append(audioPath).append("\" ");
         retVal.append(" code=\"").append(code).append("\" ");
         retVal.append(" correctResponses=\"").append(correctResponse).append("\" ");
 
