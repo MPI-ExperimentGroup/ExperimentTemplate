e961fa029c92f5dd46755981b4ee22bfd51e3648
Peter Withers <peter.withers@mpi.nl>
2018-11-23 11:57:17 +0100

    Added mediaid to audio and video features. Modified the audio player to no longer be a singleton so that multiple audio players can coexist in the same way as video. Modified the stop play pause features to address any audio or video players by their mediaid. Note that there is a logic change that adding an audio file will no longer terminate existing audio playback.

--- ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/model/FeatureAttribute.java
+++ ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/model/FeatureAttribute.java
@@ -93,7 +93,7 @@ public enum FeatureAttribute {
     offset,
     msToNext(false),
     groupId(true),
-    listenerId, threshold, maximum, minimum,
+    mediaId, listenerId, threshold, maximum, minimum,
     msLabelFormat,
     animate(true), // animate currently has bounce stimuliCode or none
     minStimuliPerTag(true), // for each tag there should be at least N of each represented in the final list
