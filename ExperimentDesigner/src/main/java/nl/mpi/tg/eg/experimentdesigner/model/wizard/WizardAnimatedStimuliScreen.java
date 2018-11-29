9fe1997624c44e4564058c930eaa9d12733b5ad4
Peter Withers <peter.withers@mpi.nl>
2018-11-29 11:57:36 +0100

    Added mediaid to audio and video features. Modified the audio player to no longer be a singleton so that multiple audio players can coexist in the same way as video. Modified the stop play pause features to address any audio or video players by their mediaid. Note that there is a logic change that adding an audio file will no longer terminate existing audio playback.

--- ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/model/wizard/WizardAnimatedStimuliScreen.java
+++ ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/model/wizard/WizardAnimatedStimuliScreen.java
@@ -144,6 +144,7 @@ public class WizardAnimatedStimuliScreen extends AbstractWizardScreen {
         startRecorderFeature.addFeatureAttributes(FeatureAttribute.recordingFormat, "wav");
         startRecorderFeature.addFeatureAttributes(FeatureAttribute.filePerStimulus, "false");
         startRecorderFeature.addFeatureAttributes(FeatureAttribute.eventTag, storedWizardScreenData.getScreenTitle());
+        startRecorderFeature.addFeatureAttributes(FeatureAttribute.mediaId, "mediaId");
         startRecorderFeature.addFeature(FeatureType.onError, null);
         startRecorderFeature.addFeature(FeatureType.onSuccess, null);
         hasMoreStimulusFeature.getPresenterFeatureList().add(startRecorderFeature);
