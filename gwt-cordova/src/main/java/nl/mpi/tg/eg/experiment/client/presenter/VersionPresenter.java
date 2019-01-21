9d5ece1c6b6f18dcd31854fe5631b826bd5adef3
Peter Withers <peter.withers@mpi.nl>
2019-01-21 16:01:14 +0100

    Updated transmitResults tag to be usable in the transmission presenter rather than just the stimulus presenter. (LiI) Updated transmitResults, htmlTokenText, getMetadataValue and setMetadataValue to be usable in any presenter type, subject to the available data in that presenter type. (LiI)

--- gwt-cordova/src/main/java/nl/mpi/tg/eg/experiment/client/presenter/VersionPresenter.java
+++ gwt-cordova/src/main/java/nl/mpi/tg/eg/experiment/client/presenter/VersionPresenter.java
@@ -21,6 +21,9 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
 import nl.mpi.tg.eg.experiment.client.Version;
 import nl.mpi.tg.eg.experiment.client.listener.AppEventListner;
+import nl.mpi.tg.eg.experiment.client.model.UserResults;
+import nl.mpi.tg.eg.experiment.client.service.LocalStorage;
+import nl.mpi.tg.eg.experiment.client.service.TimerService;
 import nl.mpi.tg.eg.experiment.client.view.ComplexView;
 
 /**
@@ -31,8 +34,8 @@ public class VersionPresenter extends AbstractPresenter {
 
     private final Version version = GWT.create(Version.class);
 
-    public VersionPresenter(RootLayoutPanel widgetTag) {
-        super(widgetTag, new ComplexView());
+    public VersionPresenter(RootLayoutPanel widgetTag, UserResults userResults, final LocalStorage localStorage, final TimerService timerService) {
+        super(widgetTag, new ComplexView(), userResults, localStorage, timerService);
     }
 
     @Override
