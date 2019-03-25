3af83eeeff41980b3b9fdc34bfcd6589a683a1f4
Peter Withers <peter.withers@mpi.nl>
2019-03-25 17:21:54 +0100

    Added checking for metadata changes and added an implicit metadata transmission when a presenter is exited, either by internal navigation or browser navigation. Resurrected the showHtmlPopup feature.

--- gwt-cordova/src/main/java/nl/mpi/tg/eg/experiment/client/presenter/LocalStoragePresenter.java
+++ gwt-cordova/src/main/java/nl/mpi/tg/eg/experiment/client/presenter/LocalStoragePresenter.java
@@ -49,7 +49,7 @@ import nl.mpi.tg.eg.experiment.client.view.TimedStimulusView;
 public abstract class LocalStoragePresenter extends AbstractTimedPresenter {
 
     public LocalStoragePresenter(RootLayoutPanel widgetTag) {
-        super(widgetTag, new TimedStimulusView(), null, null, null);
+        super(widgetTag, new TimedStimulusView(), null, null, null, null);
     }
 
     @Override
