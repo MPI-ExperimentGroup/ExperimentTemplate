78c3bd49e5f2a5c2e328fbff04adee0c7d76e25f
Peter Withers <peter.withers@mpi.nl>
2019-08-27 11:51:41 +0200

    Updated the feature data model to allow documentation strings for each attribute. Added the capacity of the feature data model to document the data type of each attribute. Added an attribute to rating button features to assign different orientations.

--- ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/model/FeatureAttribute.java
+++ ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/model/FeatureAttribute.java
@@ -17,6 +17,8 @@
  */
 package nl.mpi.tg.eg.experimentdesigner.model;
 
+import nl.mpi.tg.eg.experimentdesigner.util.AbstractSchemaGenerator.AttributeType;
+
 /**
  * this can be updated with the output of: grep select=
  * ~/Documents/ExperimentTemplate/gwt-cordova/src/main/xsl/config2java.xsl
@@ -52,7 +54,7 @@ public enum FeatureAttribute {
     maxHeight,
     maxWidth,
     align,
-    orientation(true, new String[]{"horizontal", "vertical", "none"}),
+    orientation(null, true, new String[]{"horizontal", "vertical", "flow"}),
     target(false), // this is probably not optional in some cases
     targetOptions(true),
     styleName(true),
@@ -61,7 +63,7 @@ public enum FeatureAttribute {
     eventTier,
     dataChannel(true),
     dataLogFormat(false),
-    filePerStimulus, // when recording audio this boolean determins if a separate recording should be made for each stimulus or one recording for the set of stimuli
+    filePerStimulus("When recording audio this boolean determins if a separate recording should be made for each stimulus or one recording for the set of stimuli"),
     eventTag(false),
     ratingLabels,
     ratingLabelLeft(true),
@@ -74,8 +76,8 @@ public enum FeatureAttribute {
     repeatRandomWindow(true), // todo: document how this works, which currently is to compare in sequence, image, audio, video and label and use the first found one as the comparitor. This could be made more explicit by adding a comparitor attribute that would be default be set to "image audio video label" for example
     adjacencyThreshold(true),
     repeatIncorrect,
-//    repeatMatching(true),
-    hotKey(true, new String[]{
+    //    repeatMatching(true),
+    hotKey(null, true, new String[]{
         "A",
         "B",
         "C",
@@ -228,7 +230,8 @@ public enum FeatureAttribute {
     downloadPermittedWindowMs,
     deviceRegex(true),
     poster,
-    autoPlay, // Modern web browsers will prevent media from playing before the user interacts with the page after each time it has loaded. If this is an issue, it can be overcome by always having a begin button, or by having a replay button in the case of an initial failure.
+    autoPlay("When true media will be played as soon as it has loaded. Modern web browsers will prevent media from playing before the user interacts with the page after each time it has loaded. If this is an issue, it can be overcome by always having a begin button, or by having a replay button in the case of an initial failure.",
+            false, new String[]{"true", "false"}),
     loop,
     columnCount,
     kintypestring,
@@ -263,20 +266,42 @@ public enum FeatureAttribute {
     scoreValue;
     final boolean isOptional;
     final String[] typeValues;
+    final AttributeType attributeType;
+    final String documentation;
 
     private FeatureAttribute() {
         this.isOptional = false;
         this.typeValues = null;
+        this.attributeType = null;
+        this.documentation = null;
+    }
+
+    private FeatureAttribute(final String documentation) {
+        this.isOptional = false;
+        this.typeValues = null;
+        this.attributeType = null;
+        this.documentation = documentation;
     }
 
     private FeatureAttribute(boolean isOptional) {
         this.isOptional = isOptional;
         this.typeValues = null;
+        this.attributeType = null;
+        this.documentation = null;
     }
 
-    private FeatureAttribute(boolean isOptional, final String[] typeValues) {
+    private FeatureAttribute(final String documentation, boolean isOptional, final String[] typeValues) {
         this.isOptional = isOptional;
         this.typeValues = typeValues;
+        this.attributeType = null;
+        this.documentation = documentation;
+    }
+
+    private FeatureAttribute(final String documentation, boolean isOptional, final AttributeType attributeType) {
+        this.isOptional = isOptional;
+        this.typeValues = null;
+        this.attributeType = attributeType;
+        this.documentation = documentation;
     }
 
     public boolean isOptional() {
@@ -285,5 +310,13 @@ public enum FeatureAttribute {
 
     public String[] getTypeValues() {
         return typeValues;
+    }
+
+    public AttributeType getType() {
+        return attributeType;
+    }
+
+    public String getDocumentation() {
+        return documentation;
     }
 }
