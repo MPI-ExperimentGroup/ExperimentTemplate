28ffe9f264c921af11ea56331168862280f004ba
Peter Withers <peter.withers@mpi.nl>
2019-09-25 11:56:51 +0200

    Update for the electron and cordova build process to include the webjars. (Electron sessions for LiI)

--- gwt-cordova/src/main/electron/setup-electron.sh
+++ gwt-cordova/src/main/electron/setup-electron.sh
@@ -24,9 +24,9 @@ pwd
 
 cp -r /init-setup-project/node_modules /target/$appname-electron/
 
-mv /target/$appname-electron/www/webjars/jquery/*/*.* /target/$appname-electron/www/webjars/jquery/
-mv /target/$appname-electron/www/webjars/stomp-websocket/*/*.* /target/$appname-electron/www/webjars/stomp-websocket/
-mv /target/$appname-electron/www/webjars/sockjs-client/*/*.* /target/$appname-electron/www/webjars/sockjs-client/
+mv /target/$appname-electron/www/webjars/jquery/*/*.* /target/$appname-electron/src/webjars/jquery/
+mv /target/$appname-electron/www/webjars/stomp-websocket/*/*.* /target/$appname-electron/src/webjars/stomp-websocket/
+mv /target/$appname-electron/www/webjars/sockjs-client/*/*.* /target/$appname-electron/src/webjars/sockjs-client/
 
 asar pack /target/$appname-electron/ ../$appname.asar
 
