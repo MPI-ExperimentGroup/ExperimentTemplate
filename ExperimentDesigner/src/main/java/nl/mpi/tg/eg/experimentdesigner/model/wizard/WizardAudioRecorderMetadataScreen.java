ba154671e25c671d3e731054bb62ae2a0e433d72
Peter Withers <peter-gthb@bambooradical.com>
2020-02-26 14:38:48 +0100

    Diverged the startAudioRecorder into startAudioRecorderWeb and startAudioRecorderApp. Changed triggerListener to triggerDefinition and trigger to triggerMatching.

--- ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/model/wizard/WizardAudioRecorderMetadataScreen.java
+++ ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/model/wizard/WizardAudioRecorderMetadataScreen.java
@@ -89,9 +89,9 @@ public class WizardAudioRecorderMetadataScreen extends AbstractWizardScreen {
         presenterFeatureList.add(loadStimuliFeature);
         final PresenterFeature hasMoreStimulusFeature = new PresenterFeature(FeatureType.hasMoreStimulus, null);
 
-        final PresenterFeature startRecorderFeature = new PresenterFeature(FeatureType.startAudioRecorder, null);
-        startRecorderFeature.addFeatureAttributes(FeatureAttribute.downloadPermittedWindowMs, "0");
-        startRecorderFeature.addFeatureAttributes(FeatureAttribute.recordingFormat, "wav");
+        final PresenterFeature startRecorderFeature = new PresenterFeature(FeatureType.startAudioRecorderApp, null);
+//        startRecorderFeature.addFeatureAttributes(FeatureAttribute.downloadPermittedWindowMs, "0");
+//        startRecorderFeature.addFeatureAttributes(FeatureAttribute.recordingFormat, "wav");
         startRecorderFeature.addFeatureAttributes(FeatureAttribute.filePerStimulus, "false");
         startRecorderFeature.addFeatureAttributes(FeatureAttribute.eventTag, "Metadata");
         startRecorderFeature.addFeatureAttributes(FeatureAttribute.mediaId, "mediaId");
