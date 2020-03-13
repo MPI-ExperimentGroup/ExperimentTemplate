e6e570bd20616e2fa8433463e8785bf437fc917b
PeterWithers <peter-gthb@bambooradical.com>
2020-03-13 12:24:19 +0100

    Added parsing of the deployment element in the experiment XML to export into the listing JSON.

--- ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/model/PublishEvents.java
+++ ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/model/PublishEvents.java
@@ -52,9 +52,6 @@ public class PublishEvents implements Serializable {
     private Date publishDate;
     @Temporal(javax.persistence.TemporalType.DATE)
     private Date expiryDate;
-
-    private String registrationUrlStaging;
-    private String registrationUrlProduction;
     private PublishState publishState;
     private boolean isWebApp;
     private boolean isDesktop;
@@ -89,22 +86,6 @@ public class PublishEvents implements Serializable {
 //    public String getExperimentDisplayName() {
 //        return experiment.getAppNameDisplay();
 //    }
-    public String getRegistrationUrlStaging() {
-        return registrationUrlStaging;
-    }
-
-    public void setRegistrationUrlStaging(String registrationUrlStaging) {
-        this.registrationUrlStaging = registrationUrlStaging;
-    }
-
-    public String getRegistrationUrlProduction() {
-        return registrationUrlProduction;
-    }
-
-    public void setRegistrationUrlProduction(String registrationUrlProduction) {
-        this.registrationUrlProduction = registrationUrlProduction;
-    }
-
     @XmlAttribute
     public PublishState getState() {
         return publishState;
