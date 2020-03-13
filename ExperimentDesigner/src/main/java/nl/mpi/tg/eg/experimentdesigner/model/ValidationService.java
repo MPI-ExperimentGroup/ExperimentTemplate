55ab89ce6cfb35761c6e568a430e697e753005d1
PeterWithers <peter-gthb@bambooradical.com>
2020-03-13 12:25:49 +0100

    Added parsing of the deployment element in the experiment XML to export into the listing JSON.

---
+++ ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/model/ValidationService.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2020 Max Planck Institute for Psycholinguistics, Nijmegen
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package nl.mpi.tg.eg.experimentdesigner.model;
+
+import javax.xml.bind.annotation.XmlAttribute;
+
+/**
+ * @since 13/03/2020 10:59 AM (creation date)
+ * @author Peter Withers <peter.withers@mpi.nl>
+ */
+public class ValidationService {
+
+    private String stagingUrl;
+    private String productionUrl;
+
+    public ValidationService() {
+    }
+
+    @XmlAttribute(name = "stagingUrl")
+    public String getRegistrationUrlStaging() {
+        return stagingUrl;
+    }
+
+    public void setRegistrationUrlStaging(String stagingUrl) {
+        this.stagingUrl = stagingUrl;
+    }
+
+    @XmlAttribute(name = "productionUrl")
+    public String getRegistrationUrlProduction() {
+        return productionUrl;
+    }
+
+    public void setRegistrationUrlProduction(String productionUrl) {
+        this.productionUrl = productionUrl;
+    }
+}
