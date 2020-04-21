792b14250a1f3b506c4a3b7c0aef5354830b590f
Peter Withers <peter-gthb@bambooradical.com>
2020-04-21 15:32:49 +0200

    Added documentation. Added clearStimulusResponse and removed clearStimulus.

--- ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/model/FeatureAttribute.java
+++ ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/model/FeatureAttribute.java
@@ -240,7 +240,7 @@ public enum FeatureAttribute {
     imageWidth,
     alternativeChoice,
     offset,
-    msToNext(false),
+    msToNext("The milliseconds to delay after completion. The resulting delay is approximate and variability should be tested in the intended environment.", false, AttributeType.xsInteger),
     groupId("This value is used in multiple ways depending on the context, to set the button group name used to enable and disable button groups, to assign the storage name for stimulus responses and the field name in the JSON data transmitted to the admin server so that multiple responses can be collected for a single stimulus.", true, AttributeType.xsString),
     mediaId(/*true*/), listenerId, threshold, maximum, minimum, average, ranges(true),
     msLabelFormat,
