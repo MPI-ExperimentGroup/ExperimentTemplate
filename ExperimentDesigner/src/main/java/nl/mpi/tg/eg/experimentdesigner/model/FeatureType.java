477b47c8876a8938df632f1d546928b5d7984194
Peter Withers <peter-gthb@bambooradical.com>
2020-09-07 17:03:22 +0200

    Updated documentation and the XML schema file. Added documentation for the use of hardwareTimeStamps.

--- ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/model/FeatureType.java
+++ ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/model/FeatureType.java
@@ -158,7 +158,7 @@ public enum FeatureType {
     gotoPresenter(false, false, new FeatureAttribute[]{target}),
     gotoNextPresenter(false, false, new FeatureAttribute[]{}),
     logTimeStamp(false, new FeatureAttribute[]{eventTag}, "Records a timestamp similar to the way image and media onLoad events etc. are logged. The logged ms is relative to the load time of the presenter.", Contitionals.none, Contitionals.none),
-    hardwareTimeStamp(false, new FeatureAttribute[]{opto1, opto2, dtmf}, "When the URL parameter &quot;hardwareTimeStamp&quot; is provided a hardware measurable marker will be produced either visually on screen or as an audible tone or both.", Contitionals.none, Contitionals.none),
+    hardwareTimeStamp(false, new FeatureAttribute[]{opto1, opto2, dtmf}, "When the URL parameter &quot;hardwareTimeStamp&quot; is provided hardware measurable markers will be produced either visually on screen or as an audible tone or both. The visual markers are in the form of two black/white squares at the top of the screen. The audio markers are in the form of tones. These markers are equivalent to &lt;logTimeStamp&gt; and should be used in a similar way.", Contitionals.none, Contitionals.none),
     // todo: document audioButton which fires the played event once and only once after the first playback finishes
     audioButton(false, new FeatureAttribute[]{eventTag, dataChannel, poster, autoPlay, hotKey, styleName, src, groupId}, false, false, false, Contitionals.hasMediaPlayback, Contitionals.none), // todo: add loading complete, failed and additinally for time based media, playback complete Contitionals.requiresLoading, isTimeBasedMedia
     preloadAllStimuli(false, null, true, false, false, Contitionals.hasErrorSuccess, Contitionals.none),
