23283eee355d60603384b822b54232b19c1a0ae7
Peter Withers <peter-gthb@bambooradical.com>
2020-10-12 16:43:33 +0200

    Updated the XML template to JSON and update the expected test data.

--- ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/template/XmlJsonTemplate.java
+++ ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/template/XmlJsonTemplate.java
@@ -27,7 +27,11 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPathExpressionException;
 import nl.mpi.tg.eg.experimentdesigner.util.XpathExperimentException;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
 /**
@@ -36,8 +40,68 @@ import org.xml.sax.SAXException;
  */
 public class XmlJsonTemplate {
 
-    private void appendJsonContents(final Document xmlDocument, final BufferedWriter bufferedWriter) throws IOException {
-        bufferedWriter.append("todo: add JSON contents from XML containing template elements.");
+    private void appendJsonContents(final Node templateElement, final JSONObject jSONObject, final BufferedWriter bufferedWriter) throws IOException {
+        Node currentElement = templateElement;
+        while (currentElement != null) {
+            System.out.println(currentElement.getLocalName());
+            System.out.println(currentElement.getNodeName());
+            if ("template".equals(currentElement.getNodeName())) {
+                final NamedNodeMap attributes = currentElement.getAttributes();
+                final Node jsonPath = attributes.getNamedItem("jsonPath");
+                final Node ifJsonPath = attributes.getNamedItem("ifJsonPath");
+                final Node defaultValue = attributes.getNamedItem("defaultValue");
+                final Node attributeName = attributes.getNamedItem("attributeName");
+                final Node metadataJsonPath = attributes.getNamedItem("metadataJsonPath");
+                final Node stimuliJsonPath = attributes.getNamedItem("stimuliJsonPath");
+                final Node description = attributes.getNamedItem("description");
+                final JSONObject jsonChildObject = new JSONObject();
+                if (description != null) {
+                    //bufferedWriter.append("\n// " + description.getNodeValue() + "\n");
+                    jsonChildObject.put("description", description.getNodeValue());
+                }
+                if (jsonPath != null) {
+                    //bufferedWriter.append("\n\"" + jsonPath.getNodeValue() + "\": ");
+//                    jSONObject.put(jsonPath.getNodeValue(), "value");
+                    jSONObject.put(jsonPath.getNodeValue(), jsonChildObject);
+                }
+                if (ifJsonPath != null) {
+                    /*bufferedWriter.append("\n\"" + ifJsonPath.getNodeValue() + "\": ");
+                    if (defaultValue != null) {
+                        bufferedWriter.append("\"" + defaultValue.getNodeValue() + "\";\n");
+                    } else {
+                        bufferedWriter.append("\"\";\n");
+                    }*/
+                    jSONObject.put(ifJsonPath.getNodeValue(), jsonChildObject);
+                    jsonChildObject.put("value", true);
+                }
+                if (metadataJsonPath != null) {
+                    //bufferedWriter.append("\n\"" + metadataJsonPath.getNodeValue() + "\": ");
+//                    jSONObject.put(metadataJsonPath.getNodeValue(), "value");
+                    jSONObject.put(metadataJsonPath.getNodeValue(), jsonChildObject);
+                    jsonChildObject.put("metadata", new JSONArray());
+                }
+                if (stimuliJsonPath != null) {
+                    //bufferedWriter.append("\n\"" + stimuliJsonPath.getNodeValue() + "\": ");
+//                    jSONObject.put(stimuliJsonPath.getNodeValue(), "value");
+                    jSONObject.put(stimuliJsonPath.getNodeValue(), jsonChildObject);
+                    jsonChildObject.put("stimuli", new JSONArray());
+                }
+                if (attributeName != null) {
+                    final NamedNodeMap parentElementAttributes = currentElement.getParentNode().getAttributes();
+                    final Node attributeNameValue = parentElementAttributes.getNamedItem(attributeName.getNodeValue());
+                    if (attributeNameValue != null) {
+                        //bufferedWriter.append("\"" + attributeNameValue.getNodeValue() + "\";");
+                        jsonChildObject.put("value", attributeNameValue.getNodeValue());
+                    } else {
+                        //bufferedWriter.append("\"no attribute value found for: " + attributeName.getNodeValue() + "\";\n");
+                        jsonChildObject.put("value", "");
+                    }
+                }
+            } else {
+                appendJsonContents(currentElement.getFirstChild(), jSONObject, bufferedWriter);
+            }
+            currentElement = currentElement.getNextSibling();
+        }
     }
 
     // use template.xsd
@@ -50,8 +114,12 @@ public class XmlJsonTemplate {
             DocumentBuilder builder = builderFactory.newDocumentBuilder();
             Document xmlDocument = builder.parse(fileInputStream);
             FileWriter schemaOutputWriter = new FileWriter(jsonFileOut);
-            BufferedWriter bufferedWriter = new BufferedWriter(schemaOutputWriter);
-            appendJsonContents(xmlDocument, bufferedWriter);
+            final BufferedWriter bufferedWriter = new BufferedWriter(schemaOutputWriter);
+            final JSONObject jsonObject = new JSONObject();
+            //bufferedWriter.append("{\n");
+            appendJsonContents(xmlDocument.getFirstChild(), jsonObject, bufferedWriter);
+            //bufferedWriter.append("}\n");
+            bufferedWriter.write(jsonObject.toString(3));
             bufferedWriter.flush();
             bufferedWriter.close();
         } catch (SAXException saxe) {
@@ -59,6 +127,6 @@ public class XmlJsonTemplate {
         }
     }
 
-    public void validateTemplateXML(File xmlFiles) throws IllegalArgumentException, IOException, ParserConfigurationException, SAXException, XPathExpressionException, XpathExperimentException {
+    public void validateTemplateXML(final File templateFile) throws IllegalArgumentException, IOException, ParserConfigurationException, SAXException, XPathExpressionException, XpathExperimentException {
     }
 }
