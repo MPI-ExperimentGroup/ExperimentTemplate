14a032e01790dae1213903ecc4e5ddf29ac23258
Peter Withers <peter-gthb@bambooradical.com>
2020-12-08 14:12:21 +0100

    Suppressed cut, copy and select all from the menu of the Electron app to suit the needs of BQ4. Added version info and quit accelerator.

--- gwt-cordova/src/main/electron/index.js
+++ gwt-cordova/src/main/electron/index.js
@@ -8,17 +8,28 @@ const dataSubmitUrl = '@experiment.destinationServerUrl@/@experiment.configurati
 
 const isDebugMode = (dataSubmitUrl.includes('staging.mpi.nl')) && app.commandLine.hasSwitch('debug-mode');
 
+app.setAboutPanelOptions({
+    applicationName: "Frinex",
+    applicationVersion: "@project.version@"
+});
+
 if (!isDebugMode) {
     var template = [{
+        label: "Application",
+        submenu: [
+            { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+            { type: "separator" },
+            { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+        ]}, {
         label: "Edit",
         submenu: [
             { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
             { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
             { type: "separator" },
-            { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-            { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+            //{ label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+            //{ label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
             { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-            { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+            //{ label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
         ]}
     ];
     
