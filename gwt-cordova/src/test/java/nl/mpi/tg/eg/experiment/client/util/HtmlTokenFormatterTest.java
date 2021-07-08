b652a08b07521fcaeeaa69484d4b3ae04aeaae1e
Peter Withers <peter-gthb@bambooradical.com>
2021-07-08 12:03:06 +0200

    Cleaning up the parseDDMMYYYDate and currentDateDDMMYYYY to address timezone or time of day issues.

--- gwt-cordova/src/test/java/nl/mpi/tg/eg/experiment/client/util/HtmlTokenFormatterTest.java
+++ gwt-cordova/src/test/java/nl/mpi/tg/eg/experiment/client/util/HtmlTokenFormatterTest.java
@@ -179,6 +179,14 @@ public class HtmlTokenFormatterTest {
         userData.setMetadataValue(session_step, "a_value_for_session_step");
         HtmlTokenFormatter instance = new HtmlTokenFormatter(GeneratedStimulus.values[0], localStorage, new GroupScoreService() {
 
+            public String formatDDMMYYYCurrentDate(int addDays, int addMonths, int addYears) {
+                return "##/##/####";
+            }
+
+            public Date parseDDMMYYYDate(String inputString) throws EvaluateTokensException {
+                return new Date();
+            }
+
             @Override
             public String getActiveChannel() {
                 return "ActiveChannel";
