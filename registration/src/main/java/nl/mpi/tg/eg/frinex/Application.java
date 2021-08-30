88b1c6a2c2dc496ed872c17c8bd7ffe2b69ea7bf
Peter Withers <peter-gthb@bambooradical.com>
2021-08-30 15:52:04 +0200

    Switch to the single @SpringBootApplication annotation.

--- registration/src/main/java/nl/mpi/tg/eg/frinex/Application.java
+++ registration/src/main/java/nl/mpi/tg/eg/frinex/Application.java
@@ -18,21 +18,16 @@
 package nl.mpi.tg.eg.frinex;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.WebApplicationContext;
 
 /**
  * @since Jun 29, 2015 4:32:46 PM (creation date)
  * @author Peter Withers <peter.withers@mpi.nl>
  */
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
-//@EnableEntityLinks
+@SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
