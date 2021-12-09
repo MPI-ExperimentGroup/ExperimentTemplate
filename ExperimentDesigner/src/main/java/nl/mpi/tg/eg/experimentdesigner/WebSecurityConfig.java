ddb0f425e9b9264c2d1a9aca66f0da8f4d0e9fc5
Peter Withers <peter-gthb@bambooradical.com>
2021-12-09 10:07:26 +0100

    Starting to add the template preview to the wizaed.

--- ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/WebSecurityConfig.java
+++ ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/WebSecurityConfig.java
@@ -60,6 +60,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .permitAll();
+                http.antMatcher("/previewframe").antMatcher("/template_example/**").headers().frameOptions().sameOrigin();
     }
 
     @Override
