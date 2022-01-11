76a9b9adfba5210e73b5b9f46a85ab25b260717a
Peter Withers <peter-gthb@bambooradical.com>
2022-01-11 16:58:45 +0100

    Starting to add template XML.

---
+++ ExperimentDesigner/src/main/resources/frinex-templates/bootstrapxmltemplates.sh
@@ -0,0 +1,272 @@
+#    echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo>
+#        <templateCategory>Example</templateCategory>
+#        <templateCategory>Text Entry</templateCategory>
+#        <templateCategory>Textual Stimuli</templateCategory>
+#        <templateCategory>Sentence Completion</templateCategory>
+#    </templateInfo>
+
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Stairs4Words \(receptive vocabulary\)\""\
+" dutch_name=\"Stairs4Words\""\
+" duration=\"7\""\
+" category=\"Linguistic experience\""\
+" description=\"Adaptive vocabulary test based on a Yes/No decision task.\""\
+" /><stimuli/></experiment>" > template_mpistairs4words2.xml
+
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Antonym production \(productive vocabulary\)\""\
+" dutch_name=\"Tegenstellingen\""\
+" duration=\"5\""\
+" category=\"Linguistic experience\""\
+" description=\"Participants hear a spoken word and are instructed to produce its antonym.\""\
+" /><stimuli/></experiment>" > template_mpiantonymsproduction.xml
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Idiom recognition\""\
+" dutch_name=\"Uitdrukkingen\""\
+" duration=\"3\""\
+" category=\"Linguistic experience\""\
+" description=\"Participants select the correct meaning for an idiomatic expression among four alternatives.\""\
+" /><stimuli/></experiment>" > template_mpiidioms.xml
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Spelling test\""\
+" dutch_name=\"Spellingtest\""\
+" duration=\"5\""\
+" category=\"Linguistic experience\""\
+" description=\"Participants identify incorrectly spelled words in a list of 60 words \(half of which are spelled incorrectly\).\""\
+" /><stimuli/></experiment>" > template_mpispellingtest.xml
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Author recognition test\""\
+" dutch_name=\"Auteurstest\""\
+" duration=\"5\""\
+" category=\"Linguistic experience\""\
+" description=\"Participants identify fictional writers in a list of 132 names \(90 are writers\).\""\
+" /><stimuli/></experiment>" > template_mpiauteurstest.xml
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Prescriptive grammar test\""\
+" dutch_name=\"Grammaticatest\""\
+" duration=\"10\""\
+" category=\"Linguistic experience\""\
+" description=\"Participants carry out grammaticality judgements on spoken sentences featuring morpho-syntactic constructions known to be difficult for adult speakers of Dutch \(e.g., ik vs. mij, als vs. dan, ze vs. hun\).\""\
+" /><stimuli/></experiment>" > template_mpiprescriptivegrammar.xml
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Auditory simple reaction time test\""\
+" dutch_name=\"Auditieve reactietijdtaak 1\""\
+" duration=\"3\""\
+" category=\"Processing speed\""\
+" description=\"Participants respond as quickly as possible to the presentation of a beep by pressing the space bar.\""\
+" /><stimuli/></experiment>" > template_mpiausimplereactiontime.xml
+
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Auditory choice reaction time test\""\
+" dutch_name=\"Auditieve reactietijdtaak 2\""\
+" duration=\"4\""\
+" category=\"Processing speed\""\
+" description=\"Participants respond as quickly as possible to the presentation of one of two beeps \(high or low\) by pressing one of two buttons.\""\
+" /><stimuli/></experiment>" > template_mpiauchoicereactiontime.xml
+
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Letter comparison test\""\
+" dutch_name=\"Lettervergelijkingstest\""\
+" duration=\"5\""\
+" category=\"Processing speed\""\
+" description=\"Participants indicate as quickly as possible whether two letter strings are identical by pressing one of two buttons.\""\
+" /><stimuli/></experiment>" > template_mpilettercomparison.xml
+
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Visual simple reaction time test\""\
+" dutch_name=\"Visuele reactietijdtaak 1\""\
+" duration=\"3\""\
+" category=\"Processing speed\""\
+" description=\"Participants respond as quickly as possible to the presentation of a geometrical shape by pressing the space bar.\""\
+" /><stimuli/></experiment>" > template_mpivisimplereactiontime.xml
+
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Visual choice reaction time test\""\
+" dutch_name=\"Visuele reactietijdtaak 2\""\
+" duration=\"4\""\
+" category=\"Processing speed\""\
+" description=\"Participants respond as quickly as possible to the presentation of one of two geometrical shapes by pressing one of two buttons.\""\
+" /><stimuli/></experiment>" > template_mpivichoicereactiontime.xml
+
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Digit span\""\
+" dutch_name=\"Cijferreeksen\""\
+" duration=\"7\""\
+" category=\"Working memory\""\
+" description=\"Participants are instructed to recall sequences of digits in the order they were encountered \(forward version\) or in the reversed order \(backward version\).\""\
+" /><stimuli/></experiment>" > template_mpidigitspan.xml
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Corsi block clicking\""\
+" dutch_name=\"Corsi block clicking test\""\
+" duration=\"7\""\
+" category=\"Working memory\""\
+" description=\"Participants are instructed to recall sequences of identical spatially separated blocks by clicking on them in the order they were encountered \(forward version\) and in the reversed order \(backward version\).\""\
+" /><stimuli/></experiment>" > template_mpicorsi.xml
+
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Picture naming\""\
+" dutch_name=\"Plaatjes benoemen\""\
+" duration=\"7\""\
+" category=\"Word production\""\
+" description=\"Participants name pictures whose names vary in word frequency as quickly as possible.\""\
+" /><stimuli/></experiment>" > template_mpipicturenaming.xml
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Rapid automatized naming\""\
+" dutch_name=\"Plaatjesseries benoemen\""\
+" duration=\"7\""\
+" category=\"Word production\""\
+" description=\"Participants are familiarized with four sets of five objects whose names vary orthogonally in frequency and neighbourhood density. Each object set is arranged in an array consisting of five rows of six objects. Participants are instructed to name all objects in the array as quickly as possible.\""\
+" /><stimuli/></experiment>" > template_mpirapidautomatizednaming.xml
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Verbal fluency\""\
+" dutch_name=\"Woorden opnoemen\""\
+" duration=\"5\""\
+" category=\"Word production\""\
+" description=\"Participants name as many words as possible belonging to pre-specified categories \(semantic part\) and starting with letters provided ahead of time \(phonological version\) within one minute.\""\
+" /><stimuli/></experiment>" > template_mpiverbalfluency.xml
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Maximal speech rate\""\
+" dutch_name=\"Maximal speech rate\""\
+" duration=\"3\""\
+" category=\"Word production\""\
+" description=\"Participants are instructed to name the months of the year as quickly as possible.\""\
+" /><stimuli/></experiment>" > template_mpimaximalspeechrate.xml
+
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Phrase generation\""\
+" dutch_name=\"Woorden en phrasen produceren\""\
+" duration=\"10\""\
+" category=\"Sentence production\""\
+" description=\"Participants are familiarized with a set of 16 common objects. These objects are presented in noun/adjectival phrases of increasing difficulty, which participants are instructed to name as quickly as possible.\""\
+" /><stimuli/></experiment>" > template_mpiwords2phrases.xml
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Sentence generation \(structured\)\""\
+" dutch_name=\"Zinnen maken 1\""\
+" duration=\"12\""\
+" category=\"Sentence production\""\
+" description=\"Participants describe scenes depicting transitive actions \(intransitives serve as fillers\) as quickly as possible using pre-specified verbal material. Colour-coding of the characters in the scenes coerces the production of active and passive sentences.\""\
+" /><stimuli/></experiment>" > template_mpisentencegeneration.xml
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Sentence generation \(unstructured\)\""\
+" dutch_name=\"Zinnen maken 2\""\
+" duration=\"10\""\
+" category=\"Sentence production\""\
+" description=\"Participants describe scenes depicting transitive and intransitive actions and are free in their choice of words and syntactic structure/voice.\""\
+" /><stimuli/></experiment>" > template_mpikonopka.xml
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Spontaneous speech\""\
+" dutch_name=\"Spontane spraak\""\
+" duration=\"4\""\
+" category=\"Sentence production\""\
+" description=\"Participants speak freely for one minute about three topics provided ahead of time.\""\
+" /><stimuli/></experiment>" > template_mpispontaneousspeech.xml
+
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Monitoring in noise in non-word lists\""\
+" dutch_name=\"Woorden in ruis\""\
+" duration=\"10\""\
+" category=\"Word comprehension\""\
+" description=\"Participants monitor lists of non-words, presented in increasing levels of noise, for the occurrence of probe non-words, presented in the clear at the beginning of a trial.\""\
+" /><stimuli/></experiment>" > template_mpinonwordmonitoring.xml
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Rhyme judgement\""\
+" dutch_name=\"Rijm herkennen\""\
+" duration=\"5\""\
+" category=\"Word comprehension\""\
+" description=\"Participants are presented with two non-words in succession and are instructed to judge as quickly as possible whether both non-words rhyme.\""\
+" /><stimuli/></experiment>" > template_mpirhymes.xml
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Auditory lexical decision\""\
+" dutch_name=\"Woorden herkennen\""\
+" duration=\"7\""\
+" category=\"Word comprehension\""\
+" description=\"Participants judge the lexicality status of an auditorily presented target word as quickly as possible.\""\
+" /><stimuli/></experiment>" > template_mpilexicaldecision.xml
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Semantic categorization\""\
+" dutch_name=\"Woorden categoriseren\""\
+" duration=\"5\""\
+" category=\"Word comprehension\""\
+" description=\"Participants judge as quickly as possible whether an auditorily presented target word belongs to a pre-specified semantic category.\""\
+" /><stimuli/></experiment>" > template_mpicategorisation.xml
+
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Monitoring in noise in sentences\""\
+" dutch_name=\"Zinnen in ruis\""\
+" duration=\"10\""\
+" category=\"Sentence comprehension\""\
+" description=\"Participants monitor predictable and non-predictable sentences, presented in increasing levels of noise, for the occurrence of probe words, presented in the clear at the beginning of a trial.\""\
+" /><stimuli/></experiment>" > template_mpisentencemonitoring.xml
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Verb semantics activation during sentence comprehension\""\
+" dutch_name=\"Plaatjeskliktest 1\""\
+" duration=\"7\""\
+" category=\"Sentence comprehension\""\
+" description=\"Participants are presented with two objects on the computer screen and a spoken sentence containing a target noun, which refers to one of the two objects. In half of the sentences, the target is predictable based on verb semantics. Participants indicate by button press which of the two objects is referred to in the sentence. On predictable trials, participants may respond before target noun onset.\""\
+" /><stimuli/></experiment>" > template_mpiwerkwoorden.xml
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Gender cue identification\""\
+" dutch_name=\"Plaatjes-lidwoord test\""\
+" duration=\"10\""\
+" category=\"Sentence comprehension\""\
+" description=\"Participants indicate for 80 objects whether their names are de- or het-nouns \(both common gender in English\).\""\
+" /><stimuli/></experiment>" > template_mpigrammaticalgendercues1.xml
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Gender cue activation during sentence comprehension\""\
+" dutch_name=\"Plaatjeskliktest 2\""\
+" duration=\"10\""\
+" category=\"Sentence comprehension\""\
+" description=\"Participants are presented with two objects \(the same as in the Gender cue identification test\) on the computer screen and a spoken sentence containing a target noun, which refers to one of the two objects. In half of the sentences, the target is predictable based on a determiner expressing the grammatical gender of the target noun. Participants indicate by button press which of the two objects is referred to in the sentence. On predictable trials, participants may respond before target noun onset.\""\
+" /><stimuli/></experiment>" > template_mpigrammaticalgendercues2.xml
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Self-paced reading\""\
+" dutch_name=\"Zinnen lezen\""\
+" duration=\"5\""\
+" category=\"Sentence comprehension\""\
+" description=\"Participants read sentences of varying syntactic complexity in a self-paced fashion.\""\
+" /><stimuli/></experiment>" > template_mpiselfpacedreading.xml
+
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Dutch auditory &amp; image vocabulary test\""\
+" dutch_name=\"Woordenschattest met plaatjes\""\
+" duration=\"12\""\
+" category=\"Extra tests\""\
+" description=\"Participants hear a spoken word and select the picture associated with its meaning among four alternatives.\""\
+" /><stimuli/></experiment>" > template_mpidaivt.xml
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Matrix reasoning test\""\
+" dutch_name=\"Non-verbale IQ test\""\
+" duration=\"8\""\
+" category=\"Extra tests\""\
+" description=\"Participants indicate which of four possible shapes completes a matrix of geometric patterns.\""\
+" /><stimuli/></experiment>" > template_mpimars.xml
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"BIG 5 personality traits\""\
+" dutch_name=\"BIG 5 vragenlijst\""\
+" duration=\"5\""\
+" category=\"Extra tests\""\
+" description=\"Participants rate personality statements.\""\
+" /><stimuli/></experiment>" > template_mpibq5bfi2.xml
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Receptive vocabulary test \(multiple choice\)\""\
+" dutch_name=\"Woordenschattest \(multiple choice\)\""\
+" duration=\"7\""\
+" category=\"Extra tests\""\
+" description=\"Participants read target words \(varying in difficulty\) and select the correct meaning among four written alternatives.\""\
+" /><stimuli/></experiment>" > template_mpiwoordenschattest.xml
+
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"One minute reading test\""\
+" dutch_name=\"Een-minute leesttest\""\
+" duration=\"3\""\
+" category=\"Extra tests production\""\
+" description=\"Participants read as many words \(increasing in difficulty\) as possible within one minute.\""\
+" /><stimuli/></experiment>" > template_mpioneminutereading.xml
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><experiment><templateInfo name=\"Story reading test\""\
+" dutch_name=\"Verhaal lezen\""\
+" duration=\"5\""\
+" category=\"Extra tests production\""\
+" description=\"Participants read as much as possible of a story within three minutes.\""\
+" /><stimuli/></experiment>" > template_mpiflamingo.xml
+
