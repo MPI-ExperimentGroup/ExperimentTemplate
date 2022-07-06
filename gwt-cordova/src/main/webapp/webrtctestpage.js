b11d4ec4ee467e1d3c590e14c6205ec93ebd733c
Peter Withers <peter-gthb@bambooradical.com>
2022-07-06 16:04:32 +0200

    Setting up the WebRTC test page incorporating existing group functionality.

--- gwt-cordova/src/main/webapp/webrtctestpage.js
+++ gwt-cordova/src/main/webapp/webrtctestpage.js
@@ -33,7 +33,7 @@ function initiateConnection() {
 }
 
 function changeStream(streamOption) {
-    peerConnection.getSenders().forEach(sender => sender.track.enabled = (streamOption[sender.track.kind]) ? streamOption[track.kind] : sender.track.enabled);
+    peerConnection.getSenders().forEach(sender => sender.track.enabled = (streamOption[sender.track.kind]) ? streamOption[sender.track.kind] : sender.track.enabled);
     sendToGroup("change", streamOption);
 }
 
