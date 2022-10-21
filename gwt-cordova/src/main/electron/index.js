ecae7dd39c7be3ce05cfd9f2b7ae857daf30893b
Peter Withers <peter-gthb@bambooradical.com>
2022-10-21 15:26:15 +0200

    Setting up Camera and Microphone permission on macOS Monterey

--- gwt-cordova/src/main/electron/index.js
+++ gwt-cordova/src/main/electron/index.js
@@ -9,7 +9,7 @@ const dataSubmitUrl = '@experiment.destinationServerUrl@/@experiment.configurati
 const isDebugMode = (dataSubmitUrl.includes('staging.mpi.nl')) && app.commandLine.hasSwitch('debug-mode');
 
 // TODO: based on the experiment XML request microphone and or camera permissions
-// const microphone = systemPreferences.askForMediaAccess('microphone');
+const microphone = systemPreferences.askForMediaAccess('microphone');
 // const camera = systemPreferences.askForMediaAccess('camera');
 
 app.setAboutPanelOptions({
