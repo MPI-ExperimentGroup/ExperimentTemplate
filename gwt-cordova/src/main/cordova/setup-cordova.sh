e850500350ee4ad38559962569aae123f81c2d9e
Peter Withers <peter.withers@mpi.nl>
2022-11-15 16:52:41 +0100

    Bumped the Android targetSdkVersion to API 32 and cordova 11 and forced the cordova-android version to 11.0.0

--- gwt-cordova/src/main/cordova/setup-cordova.sh
+++ gwt-cordova/src/main/cordova/setup-cordova.sh
@@ -54,7 +54,7 @@ fi
 
 cordova platform add ios
 #cordova platform add windows
-cordova platform add android
+cordova platform add android@11.0.0
 #cordova plugin add https://github.com/danwilson/google-analytics-plugin.git
 #cordova plugin add org.apache.cordova.device
 #cordova plugin add org.apache.cordova.statusbar
