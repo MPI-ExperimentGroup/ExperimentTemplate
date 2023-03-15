e14b4291924d931003a396f867924c93290e627a
Peter Withers <peter-gthb@bambooradical.com>
2023-03-15 12:20:23 +0100

    Addressing an issue using stimulus radio buttons in a withStimulus loop where each stimulus needs its own radio button group

--- gwt-cordova/src/main/java/nl/mpi/tg/eg/experiment/client/presenter/AbstractStimulusPresenter.java
+++ gwt-cordova/src/main/java/nl/mpi/tg/eg/experiment/client/presenter/AbstractStimulusPresenter.java
@@ -1520,11 +1520,11 @@ public abstract class AbstractStimulusPresenter extends AbstractTimedPresenter i
         } else {
             formattedGroupId = new HtmlTokenFormatter(currentStimulus, localStorage, groupParticipantService, userResults.getUserData(), timerService, metadataFieldProvider.getMetadataFieldArray()).formatString(buttonGroup);
         }
-        ratingButtons(getRatingEventListeners(appEventListener, /*stimulusProvider,*/ currentStimulus, eventTag, timedStimulusListener, currentStimulus.getUniqueId(), currentStimulus.getRatingLabels(), formattedGroupId, dataChannel), ratingLabelLeft, ratingLabelRight, false, styleName, currentStimulus.getUniqueId(), false, null, formattedGroupId, null, orientationType);
+        ratingButtons(getRatingEventListeners(appEventListener, /*stimulusProvider,*/ currentStimulus, eventTag, timedStimulusListener, currentStimulus.getUniqueId(), currentStimulus.getRatingLabels(), formattedGroupId, dataChannel), ratingLabelLeft, ratingLabelRight, false, styleName, null, false, null, formattedGroupId, null, orientationType);
     }
 
     public void stimulusRatingRadio(final AppEventListener appEventListener, /*final StimuliProvider stimulusProvider,*/ final Stimulus currentStimulus, final String buttonGroup, final TimedStimulusListener timedStimulusListener, final OrientationType orientationType, final String ratingLabelLeft, final String ratingLabelRight, final String eventTag, final String styleName, final int dataChannel, final String radioGroupName) {
-        ratingRadioButton(appEventListener, /*stimulusProvider,*/ currentStimulus, buttonGroup, timedStimulusListener, orientationType, currentStimulus.getRatingLabels(), ratingLabelLeft, ratingLabelRight, eventTag, styleName, dataChannel, radioGroupName, false);
+        ratingRadioButton(appEventListener, /*stimulusProvider,*/ currentStimulus, buttonGroup, timedStimulusListener, orientationType, currentStimulus.getRatingLabels(), ratingLabelLeft, ratingLabelRight, eventTag, styleName, dataChannel, (radioGroupName == null) ? currentStimulus.getUniqueId() : radioGroupName, false);
     }
 
     public void stimulusRatingCheckbox(final AppEventListener appEventListener, /*final StimuliProvider stimulusProvider,*/ final Stimulus currentStimulus, final String buttonGroup, final TimedStimulusListener timedStimulusListener, final OrientationType orientationType, final String ratingLabelLeft, final String ratingLabelRight, final String eventTag, final String styleName, final int dataChannel, final String radioGroupName) {
