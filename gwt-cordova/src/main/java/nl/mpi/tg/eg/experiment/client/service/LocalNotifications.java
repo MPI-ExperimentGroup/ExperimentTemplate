b4a0c2da472cb008432097ee7c87fa5aeab77eb5
Peter Withers <peter.withers@mpi.nl>
2023-05-04 16:15:40 +0200

    Updating the fieldkit permissions request to be based on what is used in the XML. Updating the notifications plugin permissions request. Rolled back to the original notifications plugin. Bumped cordova to version 11.1.0

--- gwt-cordova/src/main/java/nl/mpi/tg/eg/experiment/client/service/LocalNotifications.java
+++ gwt-cordova/src/main/java/nl/mpi/tg/eg/experiment/client/service/LocalNotifications.java
@@ -205,13 +205,13 @@ public abstract class LocalNotifications {
             //console.log("$wnd: " + $wnd);
             //console.log("$wnd.plugins: " + $wnd.plugins);
             //console.log("$wnd.cordova.plugins: " + $wnd.cordova.plugins);
-            $wnd.cordova.plugins.notification.local.hasPermission(function (granted) {
-                if (granted) {
+            // $wnd.cordova.plugins.notification.local.hasPermission(function (granted) {
+                // if (granted) {
                     localNotifications.@nl.mpi.tg.eg.experiment.client.service.LocalNotifications::setNotification(Ljava/lang/String;Ljava/lang/String;Lcom/google/gwt/core/client/JavaScriptObject;Ljava/lang/String;)(notificationTitle, notificationText, notificationActions, notificationCommand);
-                } else {
-                    localNotifications.@nl.mpi.tg.eg.experiment.client.service.LocalNotifications::setNotificationFailed()();
-                }
-            });
+                // } else {
+                //     localNotifications.@nl.mpi.tg.eg.experiment.client.service.LocalNotifications::setNotificationFailed()();
+                // }
+            // });
         } else {
             localNotifications.@nl.mpi.tg.eg.experiment.client.service.LocalNotifications::setNotificationFailed()();
         }
