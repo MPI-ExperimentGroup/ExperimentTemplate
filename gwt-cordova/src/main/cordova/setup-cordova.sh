c5901000a2dbbb2ba15268e97efd79a6e3e31ca0
Peter Withers <peter-gthb@bambooradical.com>
2023-05-15 14:53:02 +0200

    Updating the fieldkit permissions request to be based on what is used in the XML. Updating the notifications plugin permissions request. Changed the notifications plugin again. Bumped cordova to version 11.1.0

--- gwt-cordova/src/main/cordova/setup-cordova.sh
+++ gwt-cordova/src/main/cordova/setup-cordova.sh
@@ -98,11 +98,11 @@ if egrep -q "requestNotification" www/@experiment.configuration.name@.xml; then
     # field kit is included here because it is requesting notification permissions correctly
     cordova plugin add /ExperimentTemplate/FieldKitRecorder/
     #cordova plugin add cordova-plugin-local-notification
-    cordova plugin add https://github.com/katzer/cordova-plugin-local-notifications.git
+    # cordova plugin add https://github.com/katzer/cordova-plugin-local-notifications.git
     # updating the notifications plugin because Android now requires FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
     # cordova plugin add https://github.com/Roma-F/cordova-plugin-local-notifications-PF.git
     # cordova plugin add https://github.com/amataslovers/cordova-plugin-local-notifications.git
-    # cordova plugin add https://github.com/fquirin/cordova-plugin-local-notifications.git
+    cordova plugin add https://github.com/fquirin/cordova-plugin-local-notifications.git
 else
     echo false > requestNotification-false.txt
     echo "requestNotification false"
