a6457d0941de4f9848e62c0ebc18ed7b41072044
Peter Withers <peter.withers@mpi.nl>
2023-06-13 17:18:49 +0200

    Setting up separate receive channel elements for each sending participant

--- gwt-cordova/src/main/java/nl/mpi/tg/eg/experiment/client/service/GroupStreamHandler.java
+++ gwt-cordova/src/main/java/nl/mpi/tg/eg/experiment/client/service/GroupStreamHandler.java
@@ -122,7 +122,7 @@ public abstract class GroupStreamHandler {
                         groupStreamHandler.@nl.mpi.tg.eg.experiment.client.service.GroupStreamHandler::initiateConnection(Ljava/lang/String;Ljava/lang/Integer;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)(stunServer, originPhase, userId, groupId, groupUUID, memberCode, screenId);
                         $wnd.createOffer($wnd.peerConnection,
                             function(offer) {
-                                groupStreamHandler.@nl.mpi.tg.eg.experiment.client.service.GroupStreamHandler::messageGroup(Ljava/lang/String;Ljava/lang/String;Ljava/lang/Integer;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)("offer", JSON.stringify({ type: 'offer', sdp: offer.sdp }), originPhase, userId, groupId, groupUUID, memberCode, screenId);
+                                groupStreamHandler.@nl.mpi.tg.eg.experiment.client.service.GroupStreamHandler::messageGroup(Ljava/lang/String;Ljava/lang/String;Ljava/lang/Integer;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)("offer", JSON.stringify({ type: 'offer', sdp: offer.sdp, 'userId': userId, 'mediaType': contentData.messageData }), originPhase, userId, groupId, groupUUID, memberCode, screenId);
                             }, function(error) {
                                 console.log(error.message);
                                 groupStreamHandler.@nl.mpi.tg.eg.experiment.client.service.GroupStreamHandler::disconnectStreams(Ljava/lang/Integer;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)(originPhase, userId, groupId, groupUUID, memberCode, screenId);
