80fb4b423275f5f3fa89a2ff5dcba5c4bb798fca
Peter Withers <peter-gthb@bambooradical.com>
2023-10-16 15:29:44 +0200

    Adding a stimulus event stopwatch feature so that each stimulus response can have a number of stimulus time periods recorded.

--- registration/src/main/java/nl/mpi/tg/eg/frinex/model/StimulusResponse.java
+++ registration/src/main/java/nl/mpi/tg/eg/frinex/model/StimulusResponse.java
@@ -22,6 +22,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -66,6 +67,7 @@ public class StimulusResponse {
     private int maxCorrectStreak;
     private int maxErrorStreak;
     private int maxPotentialScore;
+    @ElementCollection
     private List<EventTime> eventTimes; 
     
     public StimulusResponse() {
