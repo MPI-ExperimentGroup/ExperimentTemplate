e422ac3ad991039f1575327c738e89b3997fb7ca
Peter Withers <peter-gthb@bambooradical.com>
2024-01-17 15:50:52 +0100

    Adding single direction streaming WebRTC for group experiments.

--- gwt-cordova/src/main/java/nl/mpi/tg/eg/frinex/sharedobjects/StreamMessage.java
+++ gwt-cordova/src/main/java/nl/mpi/tg/eg/frinex/sharedobjects/StreamMessage.java
@@ -28,7 +28,7 @@ import nl.mpi.tg.eg.experiment.client.model.UserId;
 public class StreamMessage {
 
     public enum StreamMessageState {
-        offer, answer, candidate, ready, refresh, disconnect
+        offer, answer, candidate, ready, refresh, permissions, failed, disconnect
     };
 
     public enum StreamMessageType {
