b2ebf1f3c34e632fc4cf721cbb785d9b279410cd
Peter Withers <peter.withers@mpi.nl>
2024-08-29 15:21:13 +0200

    Setting up git clone so that users experiments can be edited and builds triggered

--- ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/controller/BuildController.java
+++ ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/controller/BuildController.java
@@ -105,7 +105,7 @@ public class BuildController {
         String repositoryNameCleaned = repositoryName.replaceAll("[^A-z0-9_\\.]", "");
         StringBuilder stringBuilder = new StringBuilder();
         ProcessBuilder builder = new ProcessBuilder(
-                "git", "checkout", "http://frinexbuild.mpi.nl/git/" + repositoryNameCleaned + ".git");
+                "git", "clone", "http://frinexbuild.mpi.nl/git/" + repositoryNameCleaned + ".git");
         builder.redirectErrorStream(true);
         builder.directory(new File("/FrinexExperiments"));
         try {
