87e89e53ec87bcd5dfa0de0c4eef5eceb4ec6482
Peter Withers <peter.withers@mpi.nl>
2024-09-03 16:00:56 +0200

    Setting up git clone so that users experiments can be edited and builds triggered

--- ExperimentDesigner/src/main/resources/static/repositoryListing.js
+++ ExperimentDesigner/src/main/resources/static/repositoryListing.js
@@ -26,48 +26,55 @@ function populateListing(repository, username) {
     $.getJSON('buildhistory.json?' + new Date().getTime(), function (data) {
         document.getElementById('repositoryDiv').innerText = repository;
         document.getElementById('usernameDiv').innerText = username;
-        var repositoryAll = !$("#repositoryAll").prop('checked');
-        var userAll = !$("#userAll").prop('checked');
         for (var keyString in data.table) {
             var experimentRow = document.getElementById(keyString + '_row');
-            if ((repositoryAll || (data.table[keyString]['_repository'] !== undefined // older listings might not have the _repository and _committer
-                && data.table[keyString]['_repository'].value === repository))
-                && (userAll || (data.table[keyString]['_committer'] !== undefined // older listings might not have the _repository and _committer
-                    && data.table[keyString]['_committer'].value === username))) {
-                if (!experimentRow) {
-                    var tableRow = document.createElement('tr');
-                    experimentRow = tableRow;
-                    tableRow.id = keyString + '_row';
-                    for (var cellString of ['_repository', '_clone', '_committer', '_experiment', '_date', '_edit']) {
-                        var tableCell = document.createElement('td');
-                        tableCell.id = keyString + cellString;
-                        tableRow.appendChild(tableCell);
-                    }
-                    document.getElementById('repositoryListing').appendChild(tableRow);
+            if (!experimentRow) {
+                var tableRow = document.createElement('tr');
+                experimentRow = tableRow;
+                tableRow.id = keyString + '_row';
+                for (var cellString of ['_repository', '_clone', '_committer', '_experiment', '_date', '_edit']) {
+                    var tableCell = document.createElement('td');
+                    tableCell.id = keyString + cellString;
+                    tableRow.appendChild(tableCell);
                 }
-                for (var cellString in data.table[keyString]) {
-                    if (cellString === '_date') {
-                        var currentBuildDate = new Date(data.table[keyString][cellString].value);
-                        document.getElementById(keyString + cellString).innerHTML = currentBuildDate.getFullYear() + '-' + ((currentBuildDate.getMonth() + 1 < 10) ? '0' : '') + (currentBuildDate.getMonth() + 1) + '-' + ((currentBuildDate.getDate() < 10) ? '0' : '') + currentBuildDate.getDate() + 'T' + ((currentBuildDate.getHours() < 10) ? '0' : '') + currentBuildDate.getHours() + ':' + ((currentBuildDate.getMinutes() < 10) ? '0' : '') + currentBuildDate.getMinutes() + ':' + ((currentBuildDate.getSeconds() < 10) ? '0' : '') + currentBuildDate.getSeconds();
-                        document.getElementById(keyString + '_edit').innerHTML =
-                            '<a href=\'/blocks/' + data.table[keyString]['_experiment'].value + '\'>preview</a>';
-                        if (data.table[keyString]['_repository']) {
-                            const repositoryName = /\/git\/([A-z0-9_]*).git/.exec(data.table[keyString]['_repository'].value);
-                            if (repositoryName) {
-                                document.getElementById(keyString + '_clone').innerHTML =
-                                    ((repositoryName.length > 1) ? '<a href=\'/repository/clone/' + repositoryName[1] + '\'>clone</a>' : '');
-                            }
+                document.getElementById('repositoryListing').appendChild(tableRow);
+            }
+            for (var cellString in data.table[keyString]) {
+                if (cellString === '_date') {
+                    var currentBuildDate = new Date(data.table[keyString][cellString].value);
+                    document.getElementById(keyString + cellString).innerHTML = currentBuildDate.getFullYear() + '-' + ((currentBuildDate.getMonth() + 1 < 10) ? '0' : '') + (currentBuildDate.getMonth() + 1) + '-' + ((currentBuildDate.getDate() < 10) ? '0' : '') + currentBuildDate.getDate() + 'T' + ((currentBuildDate.getHours() < 10) ? '0' : '') + currentBuildDate.getHours() + ':' + ((currentBuildDate.getMinutes() < 10) ? '0' : '') + currentBuildDate.getMinutes() + ':' + ((currentBuildDate.getSeconds() < 10) ? '0' : '') + currentBuildDate.getSeconds();
+                    document.getElementById(keyString + '_edit').innerHTML =
+                        '<a href=\'/blocks/' + data.table[keyString]['_experiment'].value + '\'>preview</a>';
+                    if (data.table[keyString]['_repository']) {
+                        const repositoryName = /\/git\/([A-z0-9_]*).git/.exec(data.table[keyString]['_repository'].value);
+                        if (repositoryName) {
+                            document.getElementById(keyString + '_clone').innerHTML =
+                                ((repositoryName.length > 1) ? '<a href=\'/repository/clone/' + repositoryName[1] + '\'>clone</a>' : '');
                         }
-                    } else if (cellString === '_repository' || cellString === '_committer' || cellString === '_experiment') {
-                        var buildTimeSting = (typeof data.table[keyString][cellString].ms !== 'undefined' && data.table[keyString][cellString].built) ? '&nbsp;(' + parseInt(data.table[keyString][cellString].ms / 60000) + ':' + ((data.table[keyString][cellString].ms / 1000 % 60 < 10) ? '0' : '') + parseInt(data.table[keyString][cellString].ms / 1000 % 60) + ')' : '';
-                        document.getElementById(keyString + cellString).innerHTML = data.table[keyString][cellString].value + buildTimeSting;
                     }
+                } else if (cellString === '_repository' || cellString === '_committer' || cellString === '_experiment') {
+                    var buildTimeSting = (typeof data.table[keyString][cellString].ms !== 'undefined' && data.table[keyString][cellString].built) ? '&nbsp;(' + parseInt(data.table[keyString][cellString].ms / 60000) + ':' + ((data.table[keyString][cellString].ms / 1000 % 60 < 10) ? '0' : '') + parseInt(data.table[keyString][cellString].ms / 1000 % 60) + ')' : '';
+                    document.getElementById(keyString + cellString).innerHTML = (data.table[keyString][cellString].value + buildTimeSting).replace(/[^0-9_-]/g, "");
                 }
-            } else if (experimentRow) {
-                experimentRow.remove();
             }
         }
+        doFilter();
         doSort();
+    });
+}
+
+function doFilter() {
+    var repositoryAll = !$("#repositoryAll").prop('checked');
+    var userAll = !$("#userAll").prop('checked');
+    $('#repositoryListing tr[id]').each(function () {
+        if ((repositoryAll || //(data.table[keyString]['_repository'] !== undefined // older listings might not have the _repository and _committer
+            $(this.id.replace(/_row$/g, "_repository").innerHTML === repository))
+            && (userAll || //(data.table[keyString]['_committer'] !== undefined // older listings might not have the _repository and _committer
+                $(this.id.replace(/_row$/g, "_committer").innerHTML === username))) {
+            $(this).show();
+        } else {
+            $(this).hide();
+        }
     });
 }
 
