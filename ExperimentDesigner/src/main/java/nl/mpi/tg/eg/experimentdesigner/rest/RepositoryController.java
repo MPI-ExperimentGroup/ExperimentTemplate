fadbc8ded544433cf1a40996aff1a8c8c50f98f0
Peter Withers <peter-gthb@bambooradical.com>
2024-09-16 12:32:31 +0200

    Setting up git clone so that users experiments can be edited and builds triggered

--- ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/rest/RepositoryController.java
+++ ExperimentDesigner/src/main/java/nl/mpi/tg/eg/experimentdesigner/rest/RepositoryController.java
@@ -62,7 +62,7 @@ public class RepositoryController {
                             "git clone http://WizardUser:$WizardUserPass@frinexbuild.mpi.nl/wizardgit/"
                             + repositoryNameCleaned + ".git &>> /FrinexExperiments/" + repositoryNameCleaned
                             // echo "done" into the log because sometimes the log file does not contain all of the git output
-                            + ".log; sleep 5; echo \"done\" >> /FrinexExperiments/" + repositoryNameCleaned
+                            + ".log; sleep 5; echo \", done.\" >> /FrinexExperiments/" + repositoryNameCleaned
                             + ".log;");
                     builder.redirectErrorStream(true);
                     builder.directory(new File("/FrinexExperiments"));
