cf9c1d85f53dda9d8551b384f301cea70cde6b06
Peter Withers <peter-gthb@bambooradical.com>
2024-11-25 15:56:26 +0100

    Adding file upload for the drag and drop of stimuli files to the repository page

--- ExperimentDesigner/src/main/resources/static/repositoryListing.js
+++ ExperimentDesigner/src/main/resources/static/repositoryListing.js
@@ -119,7 +119,7 @@ function populateMedia(repository, experiment, username) {
                         } else {
                             $("#" + keyString + "_folder").html(keyStringRaw.slice(0, lastSlash) + "/");
                             // $("#" + keyString + "_file").html(keyStringRaw.slice(lastSlash + 1));
-                            $("#" + keyString + "_file").html('<a href="/repository/' + repositoryShort + '/' + keyStringRaw + '">' + keyStringRaw + '</a>');
+                            $("#" + keyString + "_file").html('<a href="/repository/' + repositoryShort + keyStringRaw + '">' + keyStringRaw + '</a>');
                         }
                         if (/\.[Jj][Pp][Gg]$|\.[Pp][Nn][Gg]$|\.[Gg][Ii][Ff]$/.exec(keyStringRaw) != null) {
                             $("#" + keyString + "_preview").html("<img style=\"max-width: 100px;\" src=\"" + '/clone/' + repositoryShort + keyStringRaw + "\"/>");
