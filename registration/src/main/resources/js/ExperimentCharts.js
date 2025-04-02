4d1344eda97f6f6a9f423479d23d696503f64fa8
Peter Withers <peter-gthb@bambooradical.com>
2025-04-02 09:56:28 +0200

    Reducing memory use in the admin system when large amounts of data have been clollected and making the admin pages more dynamic. Adding more data types to the XML defined charts. Upgrading the admin pages to use dynamic tables

--- registration/src/main/resources/js/ExperimentCharts.js
+++ registration/src/main/resources/js/ExperimentCharts.js
@@ -208,7 +208,7 @@ function loadMore(tableId) {
                     const tagDate = moment(recordData[columnId]);
                     dataRow += "<td>" + tagDate.format("YYYY-MM-DD HH:mm:ss.SSS") + "<div class='balloontext'>" + recordData[columnId] + "</div></td>";
                 } else {
-                    dataRow += "<td>" + ((recordData[columnId] !== null) ? recordData[columnId] : "") + "</td>";
+                    dataRow += "<td>" + ((recordData[columnId] !== null) ? recordData[columnId] : "<div class='balloontext'>" + recordData[columnId] + "</div>") + "</td>";
                 }
             }
             dataRow += "</tr>";
