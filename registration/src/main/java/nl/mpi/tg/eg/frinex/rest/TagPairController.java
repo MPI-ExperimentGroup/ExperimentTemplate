a509271499f004ddba8c4cd54f3bdb084d1fa66b
Peter Withers <peter.withers@mpi.nl>
2025-05-19 15:49:38 +0200

    Reducing memory use in the admin system when large amounts of data have been clollected and making the admin pages more dynamic. Adding more data types to the XML defined charts

--- registration/src/main/java/nl/mpi/tg/eg/frinex/rest/TagPairController.java
+++ registration/src/main/java/nl/mpi/tg/eg/frinex/rest/TagPairController.java
@@ -17,13 +17,6 @@
  */
 package nl.mpi.tg.eg.frinex.rest;
 
-import java.util.ArrayList;
-import java.util.List;
-import nl.mpi.tg.eg.frinex.model.TagPairData;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
