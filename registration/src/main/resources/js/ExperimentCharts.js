ea8f9937bda9b579137dc96436fb69676aea2f78
Peter Withers <peter-gthb@bambooradical.com>
2025-06-17 13:15:19 +0200

    Reducing memory use in the admin system when large amounts of data have been clollected and making the admin pages more dynamic. Adding more data types to the XML defined charts. Upgrading the admin pages to use dynamic tables

--- registration/src/main/resources/js/ExperimentCharts.js
+++ registration/src/main/resources/js/ExperimentCharts.js
@@ -74,7 +74,7 @@ function generateChart(chartData) {
             data.datasets[0].backgroundColor.push(dataset.colour + '20');
             data.datasets[0].borderColor.push(dataset.colour + 'ff');
             const queryTable = (dataset.source === 'metadata') ? 'participants' : (dataset.source === 'tagdata') ? 'tagevents' : dataset.source;
-            $.getJSON(queryTable + '/search/countByLike?' + dataset.matching, function (responseData) {
+            $.getJSON(queryTable + '/search/countByLike?' + dataset.matching.replace(/;/, '%', function (responseData) {
                 // console.log(responseData);
                 data.datasets[0].data[dataKey] = responseData;
                 adminChart.update();
