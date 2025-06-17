d5803c5d24e3c8c2718b03aeba1b8872bfdb98e2
Peter Withers <peter-gthb@bambooradical.com>
2025-06-17 12:19:41 +0200

    Reducing memory use in the admin system when large amounts of data have been clollected and making the admin pages more dynamic. Adding more data types to the XML defined charts. Upgrading the admin pages to use dynamic tables

--- registration/src/main/java/nl/mpi/tg/eg/frinex/rest/TagPairRepository.java
+++ registration/src/main/java/nl/mpi/tg/eg/frinex/rest/TagPairRepository.java
@@ -79,6 +79,21 @@ public interface TagPairRepository extends PagingAndSortingRepository<TagPairDat
             @Param("tagValue1") String tagValue1,
             @Param("tagValue2") String tagValue2);
 
+    @Query("SELECT count(distinct new TagPairData(p.userId, p.screenName, p.dataChannel, p.eventTag, p.tagValue1, p.tagValue2, p.eventMs, p.tagDate)) FROM TagPairData p WHERE "
+        + "(:userId IS NULL OR p.userId like :userId) AND "
+        + "(:screenName IS NULL OR p.screenName like :screenName) AND "
+        + "(:dataChannel IS NULL OR p.dataChannel = :dataChannel) AND "
+        + "(:eventTag IS NULL OR p.eventTag like :eventTag) AND "
+        + "(:tagValue1 IS NULL OR p.tagValue1 like :tagValue1) AND "
+        + "(:tagValue2 IS NULL OR p.tagValue2 like :tagValue2)")
+    long countByLike(
+            @Param("userId") String userId,
+            @Param("screenName") String screenName,
+            @Param("dataChannel") Integer dataChannel,
+            @Param("eventTag") String eventTag,
+            @Param("tagValue1") String tagValue1,
+            @Param("tagValue2") String tagValue2);
+
     @Override
     @RestResource(exported = false)
     public abstract <S extends TagPairData> S save(S entity);
