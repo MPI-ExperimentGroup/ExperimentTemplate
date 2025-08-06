36655524ffd7cd0e375792b72530e2ac9a30f27c
Peter Withers <peter-gthb@bambooradical.com>
2025-08-06 11:39:38 +0200

    Adding tools for testing WebRTC with multiple webcam based group members

--- WebRTC_testing_tools/launch_mocked_webcams.sh
+++ WebRTC_testing_tools/launch_mocked_webcams.sh
@@ -10,3 +10,26 @@ if [ ! -e "videoA.y4m" ]; then
     ffmpeg -f lavfi -i "sine=frequency=800:duration=30" -af "volume=0.5" audioB.wav
     ffmpeg -f lavfi -i "sine=frequency=600:duration=30" -af "volume=0.5" audioC.wav
 fi
+
+mkdir -p /tmp/mocked-webcams-memberA /tmp/mocked-webcams-memberB /tmp/mocked-webcams-memberC
+
+"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" \
+  --user-data-dir="/tmp/mocked-webcams-memberA" \
+  --use-fake-device-for-media-stream \
+  --use-file-for-fake-video-capture="$pwd/videoA.y4m" \
+  --use-file-for-fake-audio-capture="$pwd/audioA.wav" \
+  "https://frinexstaging.mpi.nl/group_webcam_example/?mockuser=mock_cams_a&group=mock_cams&member=A" &
+
+"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" \
+  --user-data-dir="/tmp/mocked-webcams-memberB" \
+  --use-fake-device-for-media-stream \
+  --use-file-for-fake-video-capture="$pwd/videoB.y4m" \
+  --use-file-for-fake-audio-capture="$pwd/audioB.wav" \
+  "https://frinexstaging.mpi.nl/group_webcam_example/?mockuser=mock_cams_b&group=mock_cams&member=B" &
+
+"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" \
+  --user-data-dir="/tmp/mocked-webcams-memberC" \
+  --use-fake-device-for-media-stream \
+  --use-file-for-fake-video-capture="$pwd/videoC.y4m" \
+  --use-file-for-fake-audio-capture="$pwd/audioC.wav" \
+  "https://frinexstaging.mpi.nl/group_webcam_example/?mockuser=mock_cams_c&group=mock_cams&member=C" &
