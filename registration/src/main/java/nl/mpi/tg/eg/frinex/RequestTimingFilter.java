2f12baa410897d4a14987d2bc5159a589e168fe2
Peter Withers <peter-gthb@bambooradical.com>
2025-10-27 11:32:49 +0100

    Adding docker service scaling based on load

---
+++ registration/src/main/java/nl/mpi/tg/eg/frinex/RequestTimingFilter.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2025 Max Planck Institute for Psycholinguistics
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package nl.mpi.tg.eg.frinex;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+/**
+ * @since 27 Oct 2025 10:32 AM (creation date)
+ * @author Peter Withers <peter.withers@mpi.nl>
+ */
+@Component
+public class RequestTimingFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws java.io.IOException, ServletException {
+        long start = System.currentTimeMillis();
+        chain.doFilter(request, response);
+        long duration = System.currentTimeMillis() - start;
+        // ScalingRequestNotifier.recordRequestTime(duration);
+    }
+}
