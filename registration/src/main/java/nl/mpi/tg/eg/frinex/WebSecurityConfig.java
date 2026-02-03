7a11c2a3984cc8f25daee8e9bdbdc16f6264822d
Peter Withers <peter.withers@mpi.nl>
2026-02-03 15:40:50 +0100

    Adding limited heath metrics for grafana and scaling

--- registration/src/main/java/nl/mpi/tg/eg/frinex/WebSecurityConfig.java
+++ registration/src/main/java/nl/mpi/tg/eg/frinex/WebSecurityConfig.java
@@ -73,7 +73,7 @@ public class WebSecurityConfig {
         final String[] publicPaths = {
             "/login",
             "/actuator/health",
-            "/actuator/metrics",
+            "/actuator/metrics/*",
             "/assignValue",
             "/completeValue",
             "/validate",
