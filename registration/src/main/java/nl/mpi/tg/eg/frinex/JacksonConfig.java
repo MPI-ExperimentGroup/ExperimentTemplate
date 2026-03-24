51f9f4f5a0f2936b1040d849604dd01f686bd943
Peter Withers <peter-gthb@bambooradical.com>
2026-03-24 09:35:23 +0100

    Making JSON date time from older front end compatible with Instant globally

---
+++ registration/src/main/java/nl/mpi/tg/eg/frinex/JacksonConfig.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2026 Max Planck Institute for Psycholinguistics, Nijmegen
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package nl.mpi.tg.eg.frinex;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Instant;
+
+/**
+ * @since 24 MArch 2026 09:31 AM (creation date)
+ * @author Peter Withers <peter.withers@mpi.nl>
+ */
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+
+        SimpleModule module = new SimpleModule();
+        module.addDeserializer(Instant.class, new FlexibleInstantDeserializer());
+
+        mapper.registerModule(module);
+
+        return mapper;
+    }
+}
