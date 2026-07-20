13258baea42f5f3018875a0a053aaa71f8275908
Peter Withers <peter-gthb@bambooradical.com>
2026-07-20 17:35:53 +0200

    Adding avg eventId to the charts in admin pages

--- registration/src/test/java/nl/mpi/tg/eg/frinex/rest/allowValidationOnMissingValidationControllerTest.java
+++ registration/src/test/java/nl/mpi/tg/eg/frinex/rest/allowValidationOnMissingValidationControllerTest.java
@@ -1034,7 +1034,12 @@ public class allowValidationOnMissingValidationControllerTest {
             public long countByLike(String userId, String screenName, Integer dataChannel, Boolean isCorrect, String scoreGroup, String responseGroup, String stimulusId, String response) {
                 throw new UnsupportedOperationException("Not required for this test class.");
             }
-            
+
+            @Override
+            public Double avgEventTimeByLike(String eventId, String userId, String screenName, Integer dataChannel, Boolean isCorrect, String scoreGroup, String responseGroup, String stimulusId, String response) {
+                throw new UnsupportedOperationException("Not required for this test class.");
+            }
+
             @Override
             public long countByScreenNameLikeAndScoreGroupLikeAndResponseGroupLikeAndStimulusIdLikeAndResponseLikeAndIsCorrect(String screenName, String scoreGroup, String responseGroup, String stimulusId, String response, Boolean isCorrect) {
                 throw new UnsupportedOperationException("Not required for this test class.");
